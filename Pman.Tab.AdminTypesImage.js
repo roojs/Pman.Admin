7ccdcdbb86356ef2eeab32d34053162a29bad095
Alan Knowles <alan@akbkhome.com>
2010-11-10 16:03:08 +0800

    Pman.Tab.AdminTypesImage.js

--- Pman.Tab.AdminTypesImage.js
+++ Pman.Tab.AdminTypesImage.js
@@ -167,14 +167,6 @@ Pman.Tab.AdminTypesImage = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        header : 'Id',
-                        width : 75,
-                        dataIndex : 'id',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
                         header : 'Name',
                         width : 200,
                         dataIndex : 'name',
