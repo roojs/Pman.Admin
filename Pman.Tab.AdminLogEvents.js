84f4bf68ef8f9574a44212f75cb34cd75b6509cf
Alan Knowles <alan@akbkhome.com>
2010-12-10 12:06:53 +0800

    Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -170,7 +170,7 @@ Pman.Tab.AdminLogEvents = new Roo.util.Observable({
                             editable : false,
                             emptyText : "Select Action",
                             forceSelection : true,
-                            listWidth : 400,
+                            listWidth : 200,
                             loadingText : "Searching...",
                             minChars : 2,
                             name : 'person_id_name',
