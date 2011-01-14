7324509f97f4c7975a5de84898796b5f3c8878cb
Alan Knowles <alan@akbkhome.com>
2011-01-14 14:32:57 +0800

    Pman.Tab.AdminContacts.js

--- Pman.Tab.AdminContacts.js
+++ Pman.Tab.AdminContacts.js
@@ -455,6 +455,22 @@ Pman.Tab.AdminContacts = new Roo.util.Observable({
                             listeners : {
                                 click : function()
                                 {
+                                    
+                                    Pman.Dialog.PersonEdit.dialog.show( { id : 0 } , function() {
+                                        _this.grid.footer.onClick('first');
+                                   }); 
+                                }
+                            },
+                            cls : 'x-btn-text-icon',
+                            text : "Add",
+                            icon : Roo.rootURL + 'images/default/dd/drop-add.gif'
+                        },
+                        {
+                            xtype: 'Button',
+                            xns: Roo.Toolbar,
+                            listeners : {
+                                click : function()
+                                {
                                     var s = _this.grid.getSelectionModel().getSelections();
                                     if (!s.length || (s.length > 1))  {
                                         Roo.MessageBox.alert("Error", s.length ? "Select only one Row" : "Select a Row");
