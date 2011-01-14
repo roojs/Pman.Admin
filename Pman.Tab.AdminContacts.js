04292cd6ba6435e070c097176aaddbaaf020fd39
Alan Knowles <alan@akbkhome.com>
2011-01-14 14:26:31 +0800

    Pman.Tab.AdminContacts.js

--- Pman.Tab.AdminContacts.js
+++ Pman.Tab.AdminContacts.js
@@ -505,9 +505,9 @@ Pman.Tab.AdminContacts = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        header : 'Role',
-                        width : 200,
                         dataIndex : 'role',
+                        header : 'Role',
+                        width : 100,
                         renderer : function(v) { return String.format('{0}', v); }
                     },
                     {
