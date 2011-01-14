26aba504e1078d9de277aa86cf2bdf6a224bbced
Alan Knowles <alan@akbkhome.com>
2011-01-14 15:06:41 +0800

    Pman.Tab.AdminContacts.js

--- Pman.Tab.AdminContacts.js
+++ Pman.Tab.AdminContacts.js
@@ -100,7 +100,7 @@ Pman.Tab.AdminContacts = new Roo.util.Observable({
                             if (!_this.activeButton) {return; false;}
                             
                             
-                            o.params.status = _this.activeButton.pressed ? 1 : 0;
+                            o.params.status = _this.activeButton.pressed ? 0 : 1;
                             
                             if (Pman.Tab.AdminContactsGroup && Pman.Tab.AdminContactsGroup.grid) {
                                 var tms = Pman.Tab.ContactsGroup.grid.getLeftSelections();
