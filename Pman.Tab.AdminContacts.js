3407a280e6f52d43caa8733edd03878a043e4c9d
Alan Knowles <alan@akbkhome.com>
2011-01-14 14:34:59 +0800

    Pman.Tab.AdminContacts.js

--- Pman.Tab.AdminContacts.js
+++ Pman.Tab.AdminContacts.js
@@ -498,7 +498,13 @@ Pman.Tab.AdminContacts = new Roo.util.Observable({
                         },
                         {
                             xtype: 'TextField',
-                            xns: Roo.form
+                            xns: Roo.form,
+                            listeners : {
+                                render : function (_self)
+                                {
+                                
+                                }
+                            }
                         },
                         {
                             xtype: 'Button',
