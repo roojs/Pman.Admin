3443e1cd7b165858d50f1592dcd176b01b9ff69a
Alan Knowles <alan@akbkhome.com>
2011-02-23 18:18:21 +0800

    Pman.Tab.AdminPermManager.js

--- Pman.Tab.AdminPermManager.js
+++ Pman.Tab.AdminPermManager.js
@@ -255,14 +255,6 @@ Pman.Tab.AdminPermManager = new Roo.util.Observable({
                                     width : 200,
                                     dataIndex : 'name',
                                     renderer : function(v) { return String.format('{0}', v); }
-                                },
-                                {
-                                    xtype: 'ColumnModel',
-                                    xns: Roo.grid,
-                                    header : 'Leader',
-                                    width : 75,
-                                    dataIndex : 'leader',
-                                    renderer : function(v) { return String.format('{0}', v); }
                                 }
                             ]
                         }
