edc31f2e86c859f88d176dd40b6327d1c4a9af89
Alan Knowles <alan@akbkhome.com>
2011-02-25 16:29:32 +0800

    Pman.Tab.AdminProjectMembers.js

--- Pman.Tab.AdminProjectMembers.js
+++ Pman.Tab.AdminProjectMembers.js
@@ -95,7 +95,7 @@ Pman.Tab.AdminProjectMembers = new Roo.util.Observable({
                                     options.params.project_member_of = sel.data.id;
                                     break;
                                 case 'A': // not owner or staff..
-                                    options.params['!company_id'] = sel.data.owner_id;
+                                    options.params['!company_id'] = sel.data.client_id;
                                     options.params['!company_id_comptype'] ='OWNER';
                                     options.params.project_member_of = sel.data.id;        
                             }
