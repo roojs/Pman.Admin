6f84b65da4eb31673941fd2bbf3e6e760cb1f85f
Alan Knowles <alan@akbkhome.com>
2011-02-25 16:07:03 +0800

    Pman.Tab.AdminProjectMembers.js

--- Pman.Tab.AdminProjectMembers.js
+++ Pman.Tab.AdminProjectMembers.js
@@ -484,54 +484,6 @@ Pman.Tab.AdminProjectMembers = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        header : 'Role',
-                        width : 200,
-                        dataIndex : 'role',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
-                        header : 'Active',
-                        width : 75,
-                        dataIndex : 'active',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
-                        header : 'Remarks',
-                        width : 200,
-                        dataIndex : 'remarks',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
-                        header : 'Passwd',
-                        width : 200,
-                        dataIndex : 'passwd',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
-                        header : 'Owner',
-                        width : 75,
-                        dataIndex : 'owner_id',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
-                        header : 'Lang',
-                        width : 200,
-                        dataIndex : 'lang',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
                         header : 'No reset sent',
                         width : 75,
                         dataIndex : 'no_reset_sent',
