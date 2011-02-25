3016307dfdb1892bf90a264ec628d387a66bd571
Alan Knowles <alan@akbkhome.com>
2011-02-25 15:29:13 +0800

    Pman.Tab.AdminProjectManager.js

--- Pman.Tab.AdminProjectManager.js
+++ Pman.Tab.AdminProjectManager.js
@@ -592,6 +592,19 @@ Pman.Tab.AdminProjectManager = new Roo.util.Observable({
                                         },
                                         cls : 'x-btn-icon',
                                         icon : rootURL + '/Pman/templates/images/search.gif'
+                                    },
+                                    {
+                                        xtype: 'Button',
+                                        xns: Roo.Toolbar,
+                                        listeners : {
+                                            click : function (_self, e)
+                                            {
+                                            _this.searchBox.setValue('');
+                                                _this.grid.footer.onClick('first');
+                                            }
+                                        },
+                                        cls : 'x-btn-icon',
+                                        icon : rootURL + '/Pman/templates/images/edit-clear.gif'
                                     }
                                 ]
                             },
