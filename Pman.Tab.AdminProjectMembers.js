6f62d80f4bc43550ff89b2691d45e0805a9ab0ad
Alan Knowles <alan@akbkhome.com>
2011-02-25 16:28:04 +0800

    Pman.Tab.AdminProjectMembers.js

--- Pman.Tab.AdminProjectMembers.js
+++ Pman.Tab.AdminProjectMembers.js
@@ -50,8 +50,6 @@ Pman.Tab.AdminProjectMembers = new Roo.util.Observable({
             grid : {
                 xtype: 'Grid',
                 xns: Roo.grid,
-                autoExpandColumn : 'name',
-                loadMask : true,
                 listeners : {
                     render : function() 
                     {
@@ -69,6 +67,9 @@ Pman.Tab.AdminProjectMembers = new Roo.util.Observable({
                         }); 
                     }
                 },
+                autoExpandColumn : 'name',
+                loadMask : true,
+                 : '',
                 dataSource : {
                     xtype: 'Store',
                     xns: Roo.data,
