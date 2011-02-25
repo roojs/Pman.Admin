a4314d3228f0185b49b45276b1931a379fe647c7
Alan Knowles <alan@akbkhome.com>
2011-02-25 15:32:33 +0800

    Pman.Tab.AdminProjectManager.js

--- Pman.Tab.AdminProjectManager.js
+++ Pman.Tab.AdminProjectManager.js
@@ -666,14 +666,12 @@ Pman.Tab.AdminProjectManager = new Roo.util.Observable({
                                     {
                                         xtype: 'Button',
                                         xns: Roo.Toolbar,
-                                        listeners : {
-                                            render : function (_self)
-                                            {
-                                                if (st) { _this.grid.filter = 'P,N,U'; } 
-                                                _this.grid.footer.onClick('first');
-                                                 
-                                            }
-                                        },
+                                        toggle : 'function (_self)
+{
+    if (st) { _this.grid.filter = \'P,N,U\'; } 
+    _this.grid.footer.onClick(\'first\');
+     
+}',
                                         enableToggle : true,
                                         pressed : false,
                                         text : "Closed",
