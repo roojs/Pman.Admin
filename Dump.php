8e53a85331dc1b8d6418ab6f1b04633fe66ece45
Alan Knowles <alan@akbkhome.com>
2011-03-04 12:48:01 +0800

    Dump.php

---
+++ Dump.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * This should allow you to dump a series of data, so it could be restored later...
+ * The format will be an SQL file...
+ *
+ * usage:
+ *    php index.php Pman/Admin/Dump Project.id=123
+ *    php index.php Pman/Admin/Dump Project.id=123
+ *    
+ * 
+ *
+ * It should handle the following
+ *
+ * 
+ *
