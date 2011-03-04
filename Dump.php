9969858840dd06dadf19d2cb0271f8f9acb00811
Alan Knowles <alan@akbkhome.com>
2011-03-04 15:13:20 +0800

    Dump.php

--- Dump.php
+++ Dump.php
@@ -68,7 +68,7 @@ class Pman_Admin_Dump extends Pman {
             list($tbl, $key, $val) = explode(':', $s);
             $dd = DB_DataObject::factory($tbl);
             $dd->get($key,$val);
-            $this->toInsert($dd);
+            echo $this->toInsert($dd);
         }
         exit;
     }
