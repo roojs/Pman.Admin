efccd3ef60fc236f68c2f1371b6c67c29d873785
Alan Knowles <alan@akbkhome.com>
2011-03-04 14:38:49 +0800

    Dump.php

--- Dump.php
+++ Dump.php
@@ -67,8 +67,9 @@ class Pman_Admin_Dump extends Pman {
         global $_DB_DATAOBJECT;
         $do->links();; //force load
         $tn = $do->tableName();
-        print_r($_DB_DATAOBJECT['LINKS']);
-        foreach($_DB_DATAOBJECT['LINKS'] as $tbl => $links) {
+        
+        
+        foreach($_DB_DATAOBJECT['LINKS'][$do->databaseName()] as $tbl => $links) {
             foreach ($links as $tk => $kv) {
                 list($k,$v) = explode(':', $kv);
                 if ($k != $tn) {
