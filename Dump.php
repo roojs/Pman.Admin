0efdac5959bbd17ca18a110895528a4e11fefdca
Alan Knowles <alan@akbkhome.com>
2011-03-04 13:32:30 +0800

    Dump.php

--- Dump.php
+++ Dump.php
@@ -6,13 +6,17 @@
  * The format will be an SQL file...
  *
  * usage:
- *    php index.php Pman/Admin/Dump --table=Project --col=id --value=123 --delete=yes
+ *    php index.php Pman/Admin/Dump --table=Project --col=id --value=123 --delete=yes --dump-dir=...
  *
  *    Output??? Stdout..??
  *    
- * 
+ *  Basically it has to output all the records and their dependants. (parent and children)
  *
- * It should handle the following
+ *  Then when deleting it deletes record + children..
+ *
+ *
+ *  
+ * 
  *
  * 
  */
@@ -30,6 +34,19 @@ function Pman_Admin_Dump extends Pman {
     
     function get($path, $args=array())
     {
+        // since we are runnign in cli mode... we will be a bit wild and free with verification
+        $x = DB_DataObject::factory($args['table']);
+        $x->get($args['col'], $args['id']);
+        $this->out($x);
+        
+    }
+    
+    function out($do)
+    {
+        // for auto_inc column we need to use a 'set argument'...
+        
+        // for 
+        
         
         
     }
