21e85f8ebb461e0fd1339e9cb8c55df046293904
Alan Knowles <alan@akbkhome.com>
2011-03-06 11:07:12 +0800

    Pman.Tab.AdminEnums.js

--- Pman.Tab.AdminEnums.js
+++ Pman.Tab.AdminEnums.js
@@ -237,7 +237,7 @@ Pman.Tab.AdminEnums = new Roo.util.Observable({
                                 }
                             },
                             cls : 'x-btn-text-icon',
-                            text : "Add",
+                            text : "Add new type",
                             icon : Roo.rootURL + 'images/default/dd/drop-add.gif'
                         }
                     ]
