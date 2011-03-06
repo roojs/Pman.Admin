5310176a4af5316edb7c3b72d1fe75bc30cadefc
Alan Knowles <alan@akbkhome.com>
2011-03-06 10:47:58 +0800

    Pman.Tab.AdminEnums.js

--- Pman.Tab.AdminEnums.js
+++ Pman.Tab.AdminEnums.js
@@ -229,14 +229,6 @@ Pman.Tab.AdminEnums = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        header : 'Etype',
-                        width : 200,
-                        dataIndex : 'etype',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
                         header : 'Name',
                         width : 200,
                         dataIndex : 'name',
