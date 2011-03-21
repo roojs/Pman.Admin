b3ee6c756b4b17b71ea426bbfeb995a7760a8875
Alan Knowles <alan@akbkhome.com>
2011-03-21 17:00:16 +0800

    Dump.php

--- Dump.php
+++ Dump.php
@@ -91,9 +91,9 @@ class Pman_Admin_Dump extends Pman {
         $this->args = $args;
         $this->out = array();
         $this->discoverChildren($this->args['table'], $this->args['where'], true);
-        print_R($this->deletes);
-        print_r($this->dumps);
-        exit;
+        //print_R($this->deletes);
+        //print_r($this->dumps);
+        //exit;
         
         $this->discover($this->args['table'], $this->args['where'], true);
         print_r($this->dumps);
