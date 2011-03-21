4d749ead1d9261219715b6fdc7e73b5a0e991a9e
Alan Knowles <alan@akbkhome.com>
2011-03-21 15:59:47 +0800

    Dump.php

--- Dump.php
+++ Dump.php
@@ -136,7 +136,7 @@ class Pman_Admin_Dump extends Pman {
         $links = $x->links();
         $cols = array_keys($links);
         $keys = $x->keys();
-        $cols = array_push($cols, $key[0]);
+         array_push($cols, $key[0]);
          
         
         $x->selectAdd();
