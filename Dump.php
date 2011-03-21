ab1bcb5993f5c44d35052231e9f3d026f59aad3c
Alan Knowles <alan@akbkhome.com>
2011-03-21 18:36:47 +0800

    Dump.php

--- Dump.php
+++ Dump.php
@@ -124,6 +124,9 @@ class Pman_Admin_Dump extends Pman {
             if (empty($ar)) { continue; }
             echo "   " .$tbl . ' -> ' . count(array_keys($ar)) . " Records\n";
         }
+        echo "FILES:"
+        echo "   Total : ". count($this->childfiles) . " using " . floor($this->filesize/1000000) . "Mb\n";
+        
         echo "GENERATED FILES:\n";
         // summary
         echo "    ". implode("\n    ", $this->out). "\n";
