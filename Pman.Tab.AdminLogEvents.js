29442103a50296ebd796570afe56efc2fccc939c
Alan Knowles <alan@akbkhome.com>
2011-05-06 10:35:20 +0800

    Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -281,9 +281,9 @@ Pman.Tab.AdminLogEvents = new Roo.util.Observable({
                                 reader : {
                                     xtype: 'JsonReader',
                                     xns: Roo.data,
-                                    totalProperty : 'total',
-                                    root : 'data',
                                     id : 'id',
+                                    root : 'data',
+                                    totalProperty : 'total',
                                     fields : [
                                         {
                                             'name': 'id',
