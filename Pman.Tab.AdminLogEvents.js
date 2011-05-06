818e0bf06998a392318dd8aa17733c2b56066b41
Alan Knowles <alan@akbkhome.com>
2011-05-06 10:33:46 +0800

    Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -256,7 +256,7 @@ Pman.Tab.AdminLogEvents = new Roo.util.Observable({
                             qtip : "Select Person ",
                             queryParam : 'query[name]',
                             selectOnFocus : true,
-                            tpl : '<div class="x-grid-cell-text x-btn button"><b>{name}</b> </div>',
+                            tpl : '<div class="x-grid-cell-text x-btn button"><b>{name}</b> <{email}> </div>',
                             triggerAction : 'all',
                             typeAhead : true,
                             valueField : 'id',
