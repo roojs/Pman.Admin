6b9a9ce4bb99012b3db44eddbe50df18484a72f0
Alan Knowles <alan@akbkhome.com>
2011-05-12 16:18:33 +0800

    Pman.Tab.AdminOffice.js

--- Pman.Tab.AdminOffice.js
+++ Pman.Tab.AdminOffice.js
@@ -1,1 +1,353 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        modKey : '001-Pman.Tab.AdminOffice',
+        module : Pman.Tab.AdminOffice,
+        region : 'center',
+        parent : false,
+        name : "unnamed module",
+        disabled : false, 
+        permname: '' 
+    });
+});
+
+Pman.Tab.AdminOffice = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'GridPanel',
+            xns: Roo,
+            title : "Office",
+            fitToframe : true,
+            fitContainer : true,
+            tableName : 'Office',
+            background : true,
+            region : 'center',
+            listeners : {
+                activate : function() {
+                    _this.panel = this;
+                    if (_this.grid) {
+                        _this.grid.footer.onClick('first');
+                    }
+                }
+            },
+            grid : {
+                xtype: 'Grid',
+                xns: Roo.grid,
+                autoExpandColumn : 'name',
+                loadMask : true,
+                listeners : {
+                    render : function() 
+                    {
+                        _this.grid = this; 
+                        //_this.dialog = Pman.Dialog.FILL_IN
+                        if (_this.panel.active) {
+                           this.footer.onClick('first');
+                        }
+                    },
+                    rowdblclick : function (_self, rowIndex, e)
+                    {
+                        if (!_this.dialog) return;
+                        _this.dialog.show( this.getDataSource().getAt(rowIndex), function() {
+                            _this.grid.footer.onClick('first');
+                        }); 
+                    }
+                },
+                dataSource : {
+                    xtype: 'Store',
+                    xns: Roo.data,
+                    remoteSort : true,
+                    sortInfo : { field : 'name', direction: 'ASC' },
+                    proxy : {
+                        xtype: 'HttpProxy',
+                        xns: Roo.data,
+                        method : 'GET',
+                        url : baseURL + '/Roo/Office.php'
+                    },
+                    reader : {
+                        xtype: 'JsonReader',
+                        xns: Roo.data,
+                        totalProperty : 'total',
+                        root : 'data',
+                        id : 'id',
+                        fields : [
+                            {
+                                'name': 'id',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'name',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'address',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'phone',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'fax',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'email',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'role',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_code',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_name',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_remarks',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_owner_id',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id_address',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_tel',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_fax',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_email',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_id',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id_isOwner',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id_logo_id',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id_background_color',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_comptype',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_url',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_main_office_id',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id_created_by',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id_created_dt',
+                                'type': 'date'
+                            },
+                            {
+                                'name': 'company_id_updated_by',
+                                'type': 'int'
+                            },
+                            {
+                                'name': 'company_id_updated_dt',
+                                'type': 'date'
+                            },
+                            {
+                                'name': 'company_id_passwd',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_dispatch_port',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_province',
+                                'type': 'string'
+                            },
+                            {
+                                'name': 'company_id_country',
+                                'type': 'string'
+                            }
+                        ]
+                    }
+                },
+                footer : {
+                    xtype: 'PagingToolbar',
+                    xns: Roo,
+                    pageSize : 25,
+                    displayInfo : true,
+                    displayMsg : 'Displaying Office{0} - {1} of {2}',
+                    emptyMsg : 'No Office found'
+                },
+                toolbar : {
+                    xtype: 'Toolbar',
+                    xns: Roo,
+                    items : [
+                        {
+                            xtype: 'Button',
+                            xns: Roo.Toolbar,
+                            text : "Add",
+                            cls : 'x-btn-text-icon',
+                            icon : Roo.rootURL + 'images/default/dd/drop-add.gif',
+                            listeners : {
+                                click : function()
+                                {
+                                    if (!_this.dialog) return;
+                                    _this.dialog.show( { id : 0 } , function() {
+                                        _this.grid.footer.onClick('first');
+                                   }); 
+                                }
+                            }
+                        },
+                        {
+                            xtype: 'Button',
+                            xns: Roo.Toolbar,
+                            text : "Edit",
+                            cls : 'x-btn-text-icon',
+                            icon : Roo.rootURL + 'images/default/tree/leaf.gif',
+                            listeners : {
+                                click : function()
+                                {
+                                    var s = _this.grid.getSelectionModel().getSelections();
+                                    if (!s.length || (s.length > 1))  {
+                                        Roo.MessageBox.alert("Error", s.length ? "Select only one Row" : "Select a Row");
+                                        return;
+                                    }
+                                    if (!_this.dialog) return;
+                                    _this.dialog.show(s[0].data, function() {
+                                        _this.grid.footer.onClick('first');
+                                    }); 
+                                    
+                                }
+                            }
+                        },
+                        {
+                            xtype: 'Button',
+                            xns: Roo.Toolbar,
+                            text : "Delete",
+                            cls : 'x-btn-text-icon',
+                            icon : rootURL + '/Pman/templates/images/trash.gif',
+                            listeners : {
+                                click : function()
+                                {
+                                     Pman.genericDelete(_this, 'Office'); 
+                                }
+                            }
+                        }
+                    ]
+                },
+                colModel : [
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Id',
+                        width : 75,
+                        dataIndex : 'id',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    },
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Company',
+                        width : 75,
+                        dataIndex : 'company_id',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    },
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Name',
+                        width : 200,
+                        dataIndex : 'name',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    },
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Address',
+                        width : 200,
+                        dataIndex : 'address',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    },
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Phone',
+                        width : 200,
+                        dataIndex : 'phone',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    },
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Fax',
+                        width : 200,
+                        dataIndex : 'fax',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    },
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Email',
+                        width : 200,
+                        dataIndex : 'email',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    },
+                    {
+                        xtype: 'ColumnModel',
+                        xns: Roo.grid,
+                        header : 'Role',
+                        width : 200,
+                        dataIndex : 'role',
+                        renderer : function(v) { return String.format('{0}', v); }
+                    }
+                ]
+            }
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
