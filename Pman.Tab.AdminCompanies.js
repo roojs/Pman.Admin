700748e7b9c0f16a4985b7f481d3641ef758d4e2
Alan Knowles <alan@akbkhome.com>
2011-05-12 15:56:56 +0800

    Pman.Tab.AdminCompanies.js

--- Pman.Tab.AdminCompanies.js
+++ Pman.Tab.AdminCompanies.js
@@ -508,22 +508,6 @@ Pman.Tab.AdminCompanies = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        header : 'Owner',
-                        width : 75,
-                        dataIndex : 'owner_id',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
-                        header : 'Address',
-                        width : 200,
-                        dataIndex : 'address',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
                         header : 'Tel',
                         width : 200,
                         dataIndex : 'tel',
