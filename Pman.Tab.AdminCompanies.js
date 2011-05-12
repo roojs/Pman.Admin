36c4846425d6fe0cea2d3cc41cd53ba108702704
Alan Knowles <alan@akbkhome.com>
2011-05-12 16:08:36 +0800

    Pman.Tab.AdminCompanies.js

--- Pman.Tab.AdminCompanies.js
+++ Pman.Tab.AdminCompanies.js
@@ -33,6 +33,7 @@ Pman.Tab.AdminCompanies = new Roo.util.Observable({
         this.panel = parentLayout.addxtype({
             xtype: 'NestedLayoutPanel',
             xns: Roo,
+            region : 'center',
             title : "Companies",
             layout : {
                 xtype: 'BorderLayout',
