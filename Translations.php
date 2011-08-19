fba46a76996f385a7ead44ac6d8317ad3564e77c
Alan Knowles <alan@akbkhome.com>
2011-08-19 12:38:57 +0800

    Translations.php

--- Translations.php
+++ Translations.php
@@ -129,7 +129,7 @@ class Pman_Admin_Translations extends Pman
         $info = $this->moduleJavascriptFilesInfo($module);
         //print_r($info);
         $tfile =$info->basedir . '/'. $info->translation_data;
-         var_dump($tfile);
+         //var_dump($tfile);
         if (empty($tfile) || !file_exists($tfile)) {
             return array();
         }
