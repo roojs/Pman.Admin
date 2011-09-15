32bccaee29f5a85a5c3c8d9d6ad3be2e2362575d
Alan Knowles <alan@akbkhome.com>
2011-09-15 10:52:58 +0800

    Pman.Tab.AdminTranslations.js

--- Pman.Tab.AdminTranslations.js
+++ Pman.Tab.AdminTranslations.js
@@ -1,1 +1,40 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        modKey : '950-Pman.Tab.AdminTranslations',
+        module : Pman.Tab.AdminTranslations,
+        region : 'center',
+        parent : Pman.Tab.Admin,
+        name : "Admin - Translations",
+        disabled : false, 
+        permname: 'Admin.Translations' 
+    });
+});
+
+Pman.Tab.AdminTranslations = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'NestedLayoutPanel',
+            xns: Roo
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
