08d7b7dd0a6c0770949b1494bd54e5710ec9bb29
Alan Knowles <alan@roojs.com>
2011-09-28 15:24:01 +0800

    Pman.Tab.AdminWatches.bjs Pman.Tab.AdminWatches.bjs Pman.Tab.AdminWatches.js Pman.Tab.AdminWatches.js

--- Pman.Tab.AdminWatches.js
+++ Pman.Tab.AdminWatches.js
@@ -291,14 +291,6 @@ Pman.Tab.AdminWatches = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        header : 'Event',
-                        width : 200,
-                        dataIndex : 'event',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
                         header : 'Medium',
                         width : 200,
                         dataIndex : 'medium',
