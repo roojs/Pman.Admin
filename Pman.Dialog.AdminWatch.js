4bc82b7368b4f13c511c2802db8bba1f4d77462b
Alan Knowles <alan@roojs.com>
2011-09-28 16:31:52 +0800

    Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.js Pman.Dialog.AdminWatch.js Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.js Pman.Dialog.AdminWatch.js

--- Pman.Dialog.AdminWatch.js
+++ Pman.Dialog.AdminWatch.js
@@ -352,6 +352,22 @@ Pman.Dialog.AdminWatch = {
                     listeners : {
                         click : function (_self, e)
                         {
+                            // do some checks?
+                             
+                            
+                            _this.dialog.el.mask("Saving");
+                            _this.form.doAction("submit");
+                        
+                        }
+                    },
+                    text : "Save"
+                },
+                {
+                    xtype: 'Button',
+                    xns: Roo,
+                    listeners : {
+                        click : function (_self, e)
+                        {
                             _this.dialog.hide();
                         }
                     },
