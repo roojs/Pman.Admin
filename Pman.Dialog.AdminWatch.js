6a17308693d64c5def15063f520291bfb5dfe25c
Alan Knowles <alan@roojs.com>
2011-09-28 16:30:08 +0800

    Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.js Pman.Dialog.AdminWatch.js

--- Pman.Dialog.AdminWatch.js
+++ Pman.Dialog.AdminWatch.js
@@ -107,7 +107,7 @@ Pman.Dialog.AdminWatch = {
                                     listWidth : 300,
                                     loadingText : "Searching...",
                                     minChars : 2,
-                                    name : 'on_table',
+                                    name : 'ontable',
                                     pageSize : 20,
                                     qtip : "Select Action",
                                     queryParam : 'query[on_table]',
