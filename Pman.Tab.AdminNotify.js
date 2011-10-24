a4b10283e2e41b143a93c9b80b8fc864a6c4a030
Alan Knowles <alan@akbkhome.com>
2011-10-24 11:41:09 +0800

    Pman.Tab.AdminNotify.bjs Pman.Tab.AdminNotify.bjs Pman.Tab.AdminNotify.js Pman.Tab.AdminNotify.js

--- Pman.Tab.AdminNotify.js
+++ Pman.Tab.AdminNotify.js
@@ -299,7 +299,7 @@ Pman.Tab.AdminNotify = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        dataIndex : 'event_id',
+                        dataIndex : 'event_id_remarks',
                         header : 'Event',
                         width : 75,
                         renderer : function(v) { return String.format('{0}', v); }
