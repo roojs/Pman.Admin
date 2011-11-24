8fddd100ad1523f0efcf3fdeafe2c3bd06232d7e
Alan Knowles <alan@akbkhome.com>
2011-11-24 10:52:55 +0800

    Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.js Pman.Tab.AdminLogEvents.js Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.js Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -1126,7 +1126,7 @@ Pman.Tab.AdminLogEvents = new Roo.util.Observable({
                                             xtype: 'SimpleStore',
                                             xns: Roo.data,
                                             isLocal : true,
-                                            data : [ [ 'detailed', "Detailed" ] , 'summary', "Summary"] ],
+                                            data : [ [ 'detailed', "Detailed" ] , [ 'summary', "Summary"] ],
                                             fields : [ 'val', 'display']
                                         }
                                     }
