da553a4767aefcb7241c02707494e96301b8e104
Alan Knowles <alan@akbkhome.com>
2011-11-24 10:54:13 +0800

    Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.js Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -98,6 +98,8 @@ Pman.Tab.AdminLogEvents = new Roo.util.Observable({
                                         if (act.format) {
                                             o.params['query[to]'] = act.format('Y-m-d');
                                         }
+                                        o.params['query[viewtype]'] = _this.viewtype.getValue();
+                                        
                                         /*
                                         act = _this.groupedCombo.getValue();
                                         o.params['query[grouped]'] = act;
