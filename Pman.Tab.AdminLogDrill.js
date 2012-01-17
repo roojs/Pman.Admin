9296cfdb95fac3a81a91a8d38ae15ca6fa2008ca
Alan Knowles <alan@akbkhome.com>
2012-01-17 12:59:57 +0800

    Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.js Pman.Tab.AdminLogDrill.js

--- Pman.Tab.AdminLogDrill.js
+++ Pman.Tab.AdminLogDrill.js
@@ -232,7 +232,7 @@ Pman.Tab.AdminLogDrill = new Roo.util.Observable({
                                     xtype: 'ColumnModel',
                                     xns: Roo.grid,
                                     dataIndex : 'qty',
-                                    header : 'Remarks',
+                                    header : 'Changes',
                                     width : 200,
                                     renderer : function(v) { return String.format('{0}', v); }
                                 }
