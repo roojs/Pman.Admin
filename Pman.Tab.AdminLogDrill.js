e7a6839b5818ef29f0ead99bc3a0e9e8df017ab3
Alan Knowles <alan@akbkhome.com>
2012-01-17 12:58:10 +0800

    Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.js Pman.Tab.AdminLogDrill.js

--- Pman.Tab.AdminLogDrill.js
+++ Pman.Tab.AdminLogDrill.js
@@ -1,1 +1,45 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        part :  ["Admin","LogDrill"],
+        modKey : '001-Pman.Tab.AdminLogDrill',
+        module : Pman.Tab.AdminLogDrill,
+        region : '',
+        parent : false,
+        name : "unnamed module",
+        disabled : false, 
+        permname: '' 
+    });
+});
+
+Pman.Tab.AdminLogDrill = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'NestedLayoutPanel',
+            xns: Roo,
+            layout : {
+                xtype: 'BorderLayout',
+                xns: Roo
+            }
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
