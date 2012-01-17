07361f0763c2d994122e8ce6375f4903bc1467a1
Alan Knowles <alan@akbkhome.com>
2012-01-17 14:56:45 +0800

    Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.js Pman.Tab.AdminLogDrill.js

--- Pman.Tab.AdminLogDrill.js
+++ Pman.Tab.AdminLogDrill.js
@@ -887,6 +887,316 @@ Pman.Tab.AdminLogDrill = new Roo.util.Observable({
                                 }
                             ]
                         }
+                    },
+                    {
+                        xtype: 'GridPanel',
+                        xns: Roo,
+                        title : "Events",
+                        fitToframe : true,
+                        fitContainer : true,
+                        tableName : 'Events',
+                        background : true,
+                        region : 'center',
+                        listeners : {
+                            activate : function() {
+                                _this.panel = this;
+                                if (_this.grid) {
+                                    _this.grid.footer.onClick('first');
+                                }
+                            }
+                        },
+                        grid : {
+                            xtype: 'Grid',
+                            xns: Roo.grid,
+                            autoExpandColumn : 'person_name',
+                            loadMask : true,
+                            listeners : {
+                                render : function() 
+                                {
+                                    _this.grid = this; 
+                                    //_this.dialog = Pman.Dialog.FILL_IN
+                                    if (_this.panel.active) {
+                                       this.footer.onClick('first');
+                                    }
+                                },
+                                rowdblclick : function (_self, rowIndex, e)
+                                {
+                                    if (!_this.dialog) return;
+                                    _this.dialog.show( this.getDataSource().getAt(rowIndex), function() {
+                                        _this.grid.footer.onClick('first');
+                                    }); 
+                                }
+                            },
+                            dataSource : {
+                                xtype: 'Store',
+                                xns: Roo.data,
+                                remoteSort : true,
+                                sortInfo : { field : 'person_name', direction: 'ASC' },
+                                proxy : {
+                                    xtype: 'HttpProxy',
+                                    xns: Roo.data,
+                                    method : 'GET',
+                                    url : baseURL + '/Roo/Events.php'
+                                },
+                                reader : {
+                                    xtype: 'JsonReader',
+                                    xns: Roo.data,
+                                    totalProperty : 'total',
+                                    root : 'data',
+                                    id : 'id',
+                                    fields : [
+                                        {
+                                            'name': 'id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_name',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'event_when',
+                                            'type': 'date',
+                                            'dateFormat': 'Y-m-d'
+                                        },
+                                        {
+                                            'name': 'action',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'ipaddr',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'on_id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'on_table',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'remarks',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_office_id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_name',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_phone',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_fax',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_email',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_company_id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_role',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_active',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_remarks',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_passwd',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_owner_id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_lang',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_no_reset_sent',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_action_type',
+                                            'type': 'string'
+                                        },
+                                        {
+                                            'name': 'person_id_project_id',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_deleted_by',
+                                            'type': 'int'
+                                        },
+                                        {
+                                            'name': 'person_id_deleted_dt',
+                                            'type': 'date'
+                                        }
+                                    ]
+                                }
+                            },
+                            footer : {
+                                xtype: 'PagingToolbar',
+                                xns: Roo,
+                                pageSize : 25,
+                                displayInfo : true,
+                                displayMsg : "Displaying Events{0} - {1} of {2}",
+                                emptyMsg : "No Events found"
+                            },
+                            toolbar : {
+                                xtype: 'Toolbar',
+                                xns: Roo,
+                                items : [
+                                    {
+                                        xtype: 'Button',
+                                        xns: Roo.Toolbar,
+                                        text : "Add",
+                                        cls : 'x-btn-text-icon',
+                                        icon : Roo.rootURL + 'images/default/dd/drop-add.gif',
+                                        listeners : {
+                                            click : function()
+                                            {
+                                                if (!_this.dialog) return;
+                                                _this.dialog.show( { id : 0 } , function() {
+                                                    _this.grid.footer.onClick('first');
+                                               }); 
+                                            }
+                                        }
+                                    },
+                                    {
+                                        xtype: 'Button',
+                                        xns: Roo.Toolbar,
+                                        text : "Edit",
+                                        cls : 'x-btn-text-icon',
+                                        icon : Roo.rootURL + 'images/default/tree/leaf.gif',
+                                        listeners : {
+                                            click : function()
+                                            {
+                                                var s = _this.grid.getSelectionModel().getSelections();
+                                                if (!s.length || (s.length > 1))  {
+                                                    Roo.MessageBox.alert("Error", s.length ? "Select only one Row" : "Select a Row");
+                                                    return;
+                                                }
+                                                if (!_this.dialog) return;
+                                                _this.dialog.show(s[0].data, function() {
+                                                    _this.grid.footer.onClick('first');
+                                                }); 
+                                                
+                                            }
+                                        }
+                                    },
+                                    {
+                                        xtype: 'Button',
+                                        xns: Roo.Toolbar,
+                                        text : "Delete",
+                                        cls : 'x-btn-text-icon',
+                                        icon : rootURL + '/Pman/templates/images/trash.gif',
+                                        listeners : {
+                                            click : function()
+                                            {
+                                                 Pman.genericDelete(_this, 'Events'); 
+                                            }
+                                        }
+                                    }
+                                ]
+                            },
+                            colModel : [
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'Id',
+                                    width : 75,
+                                    dataIndex : 'id',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'Person name',
+                                    width : 200,
+                                    dataIndex : 'person_name',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'Event when',
+                                    width : 75,
+                                    dataIndex : 'event_when',
+                                    renderer : function(v) { return String.format('{0}', v ? v.format('d/M/Y') : ''); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'Action',
+                                    width : 200,
+                                    dataIndex : 'action',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'Ipaddr',
+                                    width : 200,
+                                    dataIndex : 'ipaddr',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'On',
+                                    width : 75,
+                                    dataIndex : 'on_id',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'On table',
+                                    width : 200,
+                                    dataIndex : 'on_table',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'Person',
+                                    width : 75,
+                                    dataIndex : 'person_id',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                },
+                                {
+                                    xtype: 'ColumnModel',
+                                    xns: Roo.grid,
+                                    header : 'Remarks',
+                                    width : 200,
+                                    dataIndex : 'remarks',
+                                    renderer : function(v) { return String.format('{0}', v); }
+                                }
+                            ]
+                        }
                     }
                 ],
                 west : {
