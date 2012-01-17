ca5765db601345b102c2fa1bd446e8b2512b1087
Alan Knowles <alan@akbkhome.com>
2012-01-17 15:40:46 +0800

    Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.bjs Pman.Tab.AdminLogDrill.js Pman.Tab.AdminLogDrill.js

--- Pman.Tab.AdminLogDrill.js
+++ Pman.Tab.AdminLogDrill.js
@@ -1402,9 +1402,9 @@ Pman.Tab.AdminLogDrill = new Roo.util.Observable({
                                 {
                                     xtype: 'ColumnModel',
                                     xns: Roo.grid,
+                                    dataIndex : 'on_day',
                                     header : 'Event when',
                                     width : 75,
-                                    dataIndex : 'event_when',
                                     renderer : function(v) { return String.format('{0}', v ? v.format('d/M/Y') : ''); }
                                 },
                                 {
