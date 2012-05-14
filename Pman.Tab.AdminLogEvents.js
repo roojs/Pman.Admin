96298e8e64bc3c7464d37e7cffcd71d6f5e9c158
Alan Knowles <alan@akbkhome.com>
2012-05-14 12:25:33 +0800

    Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -1131,7 +1131,8 @@ Pman.Tab.AdminLogEvents = new Roo.XComponent({
                                     header : 'Staff',
                                     width : 150,
                                     renderer : function(v,x,r) { 
-                                        return String.format('{0} &lt;<a href="mailto:{1}">{1}</a>&gt;', v, r.data.person_id_email); 
+                                        return String.format('{0} &lt;<a href="mailto:{1}">{1}</a>&gt;', 
+                                            v, r.data.person_table_email); 
                                     }
                                 },
                                 {
