4ced33fbfdd4127c4653d1a64a14a3a486323c13
Chris <chris@roojs.com>
2012-07-03 11:31:29 +0800

    Pman.Tab.AdminContacts.bjs Pman.Tab.AdminContacts.js

--- Pman.Tab.AdminContacts.js
+++ Pman.Tab.AdminContacts.js
@@ -81,15 +81,13 @@ Pman.Tab.AdminContacts = new Roo.XComponent({
                     listeners : {
                         beforeload : function (_self, o)
                         {
-                            if(_this.active_company_button){
-                                o.params.company_id = 0;
-                            }else{
-                                o.params['!company_id_comptype'] = 'OWNER';
-                            }
+                            
+                            o.params['!company_id_comptype'] = 'OWNER';
+                            
                             o.params['query[search]'] = _this.searchBox.getValue();
                             
                             if (!_this.activeButton) {return; false;}
-                            Roo.log(_this.active_company_button);
+                            
                             if (_this.companyCombo &&   _this.companyCombo.getValue()) {
                                 o.params.company_id =   _this.companyCombo.getValue();
                             }
