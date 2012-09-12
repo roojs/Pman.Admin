56dc405629d0c22b2d844e4644d615db0e1b8328
Chris <chris@roojs.com>
2012-09-12 17:01:03 +0800

    Pman.Tab.AdminContactsGroupRight.bjs Pman.Tab.AdminContactsGroupRight.js

--- Pman.Tab.AdminContactsGroupRight.js
+++ Pman.Tab.AdminContactsGroupRight.js
@@ -17,7 +17,7 @@ Pman.Tab.AdminContactsGroupRight = new Roo.XComponent({
         return {
             xtype: 'GridPanel',
             xns: Roo,
-            region : ''
+            region : 'center'
         };
     }
 });
