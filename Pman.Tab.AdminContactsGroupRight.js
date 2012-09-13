5bfe1136328c8e5fcb373277faa760fcbacda5d7
Chris <chris@roojs.com>
2012-09-13 14:34:24 +0800

    Pman.Tab.AdminContactsGroupRight.bjs Pman.Tab.AdminContactsGroupRight.js

--- Pman.Tab.AdminContactsGroupRight.js
+++ Pman.Tab.AdminContactsGroupRight.js
@@ -139,7 +139,7 @@ Pman.Tab.AdminContactsGroupRight = new Roo.XComponent({
                                 var k = e.data.accessmask.split('');
                                 Roo.each(k, function(ee){
                                     
-                                    e.data.set('accessmask_'+ee, 1);
+                                    e.data['accessmask_'+ee] = 1;
                                 });
                             });
                         }
