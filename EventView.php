2c18f75cbb7344df41c1864990fd69740fff5dab
Chris <chris@roojs.com>
2012-10-12 18:36:53 +0800

    EventView.php

--- EventView.php
+++ EventView.php
@@ -82,14 +82,14 @@ class Pman_Admin_EventView extends Pman
         $path = $ff->baseURL. "/Images/Thumb/150/{$ev->on_id}";
         foreach($filesJ->FILES as $f){
             $ip = $ff->Pman['event_log_dir']. "/{$user}" . date('/Y/m/d/',strtotime($ev->event_when)). $f->tmp_name;
-//            $img = getimagesize($ip);
+            $img = getimagesize($ip);
 //            header("Content-type: image/jpeg");
             //readfile($ip);
 //            addslashes($ip);
             echo "<img src=\"$path\" />";
             //$path = $ff->baseURL. "Images/Thumb/150/$ff->Pman['event_log_dir']. "/{$user}" . date('/Y/m/d/',strtotime($ev->event_when)). $f->tmp_name;
             //echo '<img src="'.$path.'" />';
-            print_r(Pman_Core_Images::replaceImg($str));
+            print_r($img);
         }
         
         
