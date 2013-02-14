fe47df8d67a5d8b9a94d08a0b0b79163bb5118f2
Alan Knowles <alan@akbkhome.com>
2013-02-14 22:31:00 +0800

    Iptables.php

--- Iptables.php
+++ Iptables.php
@@ -14,14 +14,21 @@ class Pman_Admin_Iptables extends Pman {
         if (!$this->bootLoader->cli) {
             die("cli only");
         }
-        }
+     }
     function get()
     {
         // find IP's that have been used to log in.
         // dump them to the iptables file.
         // if it's different - apply it...
+        $e = DB_DataObject::factory('Events');
+        $e->action = 'LOGIN';
+        $e->selectAdd();
+        $e->selectAdd('distinct(ipaddr) as ipaddr');
+        $ips = $e->fetchAll('ipaddr');
         
         
+         
+
         
     }
     
