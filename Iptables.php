8bacbd39f8a2e93374b362b602d67ab6a69b13b4
Alan Knowles <alan@akbkhome.com>
2013-03-29 12:53:25 +0800

    Iptables.php

--- Iptables.php
+++ Iptables.php
@@ -122,8 +122,8 @@ class Pman_Admin_Iptables extends Pman {
        // in the base firewall code.
        
         
-       //-A INPUT -p udp -m udp --dport 5432 -j postgres
-       //-A INPUT -p tcp -m tcp --dport 5432 -j postgres
+        //-A INPUT -p udp -m udp --dport 5432 -j postgres
+        //-A INPUT -p tcp -m tcp --dport 5432 -j postgres
         require_once 'System.php';
         
         $iptables = System::which('iptables');
