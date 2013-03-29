d5583236f4b035b8b900cbdb65e96c6730f2363b
Alan Knowles <alan@akbkhome.com>
2013-03-29 15:00:32 +0800

    Iptables.php

--- Iptables.php
+++ Iptables.php
@@ -169,6 +169,7 @@ class Pman_Admin_Iptables extends Pman {
             foreach($head as $k=>$v) {
                 $row[$v] = $ar[$k];
             }
+            print_r($row);
             
         }
         exit;
