bfb9d764372f7e7797ffee803d4d701c276b6fa1
Alan Knowles <alan@akbkhome.com>
2013-03-29 12:39:27 +0800

    Iptables.php

--- Iptables.php
+++ Iptables.php
@@ -97,6 +97,7 @@ class Pman_Admin_Iptables extends Pman {
         }
         $fn = tempnam(ini_get('session.save-path'), 'firewallconf');
         file_put_contents($fn, $this->output());
+        echo file_get_contents($fn);
         //`/sbin/iptables-restore < $fn`;
 
         exit;
