bb9bf541e59027fb6a6ab3917103ba431362d4f1
Alan Knowles <alan@akbkhome.com>
2013-03-29 15:50:46 +0800

    Iptables.php

--- Iptables.php
+++ Iptables.php
@@ -245,7 +245,7 @@ class Pman_Admin_Iptables extends Pman {
             }
             
             if ($old) {
-                $this->exec("{$iptables} -R postgres {$old['num']} -s {$ip}/32 -j ACCEPT --comment $comment";
+                $this->exec("{$iptables} -R postgres {$old['num']} -s {$ip}/32 -j ACCEPT --comment $comment");
                 
                 if (isset($remove[$ip])) {
                     unset($remove[$ip]);
@@ -253,7 +253,7 @@ class Pman_Admin_Iptables extends Pman {
                 continue;
             }
             
-            $this->exec("{$iptables} -I postgres {$lastrulenum} -s {$ip}/32 -j ACCEPT --comment $comment";
+            $this->exec("{$iptables} -I postgres {$lastrulenum} -s {$ip}/32 -j ACCEPT --comment $comment");
                     
             
                                    
