d9ec210ec5be68143c46f899c72766c94793363d
Chris <chris@roojs.com>
2013-07-10 19:17:32 +0800

    Pman.Tab.AdminEnums.bjs Pman.Tab.AdminEnums.js

--- Pman.Tab.AdminEnums.js
+++ Pman.Tab.AdminEnums.js
@@ -51,7 +51,8 @@ Pman.Tab.AdminEnums = new Roo.XComponent({
                     {
                         var di = this.colModel.getDataIndex(columnIndex);
                         var rec = _this.grid.ds.getAt(rowIndex);
-                        
+                        Roo.log(di)
+                        Roo.log(rec);
                         if(di == 'name' && rec.data.is_system_enum*1 == 1){
                             return;
                         }
