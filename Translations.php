4ff32d04e17549512d604082d46d032792a01a51
Alan Knowles <alan@roojs.com>
2013-08-16 14:00:13 +0800

    Translations.php

--- Translations.php
+++ Translations.php
@@ -46,7 +46,7 @@ class Pman_Admin_Translations extends Pman
         
         if (!empty($module)) {
             $this->init();
-            DB_DataObject::debugLevel(1);
+            //DB_DataObject::debugLevel(1);
             require_once 'Services/JSON.php';
             $d = DB_DataObject::factory('translations');
             $d->module = $module;
