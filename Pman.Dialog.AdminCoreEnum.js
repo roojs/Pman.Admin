9deb037441c9efb79f813c71c0517bff2a11d540
Alan Knowles <alan@roojs.com>
2013-10-15 15:18:52 +0800

    Pman.Dialog.AdminCoreEnum.bjs Pman.Dialog.AdminCoreEnum.js

--- Pman.Dialog.AdminCoreEnum.js
+++ Pman.Dialog.AdminCoreEnum.js
@@ -308,6 +308,12 @@ Pman.Dialog.AdminCoreEnum = {
                 {
                     xtype: 'Button',
                     xns: Roo,
+                    listeners : {
+                        click : function (_self, e)
+                        {
+                          _this.dialog.hide();
+                        }
+                    },
                     text : "Cancel"
                 },
                 {
