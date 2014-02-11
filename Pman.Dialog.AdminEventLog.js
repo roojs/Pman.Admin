bbe14209a673ae7a76a9e763a0e9c472980cad2d
Alan Knowles <alan@roojs.com>
2014-02-11 12:24:55 +0800

    Pman.Dialog.AdminEventLog.bjs Pman.Dialog.AdminEventLog.js

--- Pman.Dialog.AdminEventLog.js
+++ Pman.Dialog.AdminEventLog.js
@@ -30,7 +30,7 @@ Pman.Dialog.AdminEventLog = {
         this.dialog = Roo.factory({
             xtype: 'LayoutDialog',
             xns: Roo,
-            height : 800,
+            height : 500,
             width : 500,
             items : [
                 {
