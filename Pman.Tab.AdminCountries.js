bf32f3027400a80e6f53900e28f16a149eb77aaa
Chris <chris@roojs.com>
2014-03-18 18:23:05 +0800

    Pman.Tab.AdminCountries.bjs Pman.Tab.AdminCountries.js

--- Pman.Tab.AdminCountries.js
+++ Pman.Tab.AdminCountries.js
@@ -1,1 +1,22 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Pman.Tab.AdminCountries = new Roo.XComponent({
+    part     :  ["Admin","Countries"],
+    order    : '001-Pman.Tab.AdminCountries',
+    region   : 'center',
+    parent   : 'Pman.Tab.AdminContactsManager',
+    name     : "Pman.Tab.AdminCountries",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype: 'GridPanel',
+            xns: Roo
+        };
+    }
+});
