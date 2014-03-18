0d26f41d5230631c9ddfb2d10491db2c8dc5fe1e
Chris <chris@roojs.com>
2014-03-18 15:45:49 +0800

    Pman.Tab.AdminContacts.js

--- Pman.Tab.AdminContacts.js
+++ Pman.Tab.AdminContacts.js
@@ -31,7 +31,7 @@ Pman.Tab.AdminContacts = new Roo.XComponent({
             fitToframe : true,
             region : 'center',
             tableName : 'Person',
-            title : "Person123",
+            title : "Person",
             grid : {
                 xtype: 'Grid',
                 xns: Roo.grid,
