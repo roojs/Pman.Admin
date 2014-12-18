d5a38e87d40f04bf39958e33c19a397d2325bc20
Edward <edward@roojs.com>
2014-12-18 12:13:59 +0800

    Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -337,6 +337,7 @@ Pman.Tab.AdminLogEvents = new Roo.XComponent({
                                             '|xns' : 'Roo.data',
                                             data : (function() { 
                                             
+                                                Roo.log('test');
                                                 if(typeof(uiConfig.events_person_table) != 'undefined' && uiConfig.events_person_table.length){
                                                     Roo.log('got');
                                                     return [];        
