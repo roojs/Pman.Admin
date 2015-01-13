f020874e209a429a4e776d877eda0dfa48dae58b
Alan Knowles <alan@roojs.com>
2015-01-13 13:31:18 +0800

    Pman.Tab.AdminEnum.bjs Pman.Tab.AdminEnum.js

--- Pman.Tab.AdminEnum.js
+++ Pman.Tab.AdminEnum.js
@@ -857,8 +857,11 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
 
             },
             '|xns' : 'Roo',
+            fitContainer : true,
             xtype : 'NestedLayoutPanel',
+            fitToFrame : true,
             xns : Roo,
+            region : 'center',
             title : "Pulldown Options",
             items : [
 
