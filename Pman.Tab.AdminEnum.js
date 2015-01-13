2a679efd0795f9f5c83bcb3eaf93bf0625a2ac8a
Alan Knowles <alan@roojs.com>
2015-01-13 13:24:20 +0800

    Pman.Tab.AdminEnum.bjs Pman.Tab.AdminEnum.js

---
+++ Pman.Tab.AdminEnum.js
@@ -0,0 +1,47 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Roo.namespace('Pman.Tab');
+
+Pman.Tab.AdminEnum = new Roo.XComponent({
+    part     :  ["Admin", "Enum" ],
+    order    : '001-Pman.Tab.AdminEnum',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            layout : {
+                west : {
+                    '|xns' : 'Roo',
+                    xtype : 'LayoutRegion',
+                    xns : Roo
+                },
+                center : {
+                    '|xns' : 'Roo',
+                    xtype : 'LayoutRegion',
+                    xns : Roo
+                },
+                '|xns' : 'Roo',
+                xtype : 'BorderLayout',
+                xns : Roo,
+                items : [
+
+                ]
+
+            },
+            '|xns' : 'Roo',
+            xtype : 'NestedLayoutPanel',
+            xns : Roo,
+            items : [
+
+            ]
+
+        };    }
+});
