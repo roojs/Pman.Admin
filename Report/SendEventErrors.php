2c57268e8f931668aeab6194ad99b2ddace74555
edward <edward@roojs.com>
2016-02-05 11:42:33 +0800

    Report/SendEventErrors.php

--- Report/SendEventErrors.php
+++ Report/SendEventErrors.php
@@ -23,7 +23,7 @@ class Pman_Admin_Report_SendEventErrors extends Pman_Roo
     
     function get($args, $opts)
     {
-        print_r($args);exit;
+        print_r($opts);exit;
         
         $this->transObj = DB_DataObject::Factory('core_enum');
         
