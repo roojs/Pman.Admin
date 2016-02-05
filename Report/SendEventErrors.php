f88572682a9a11e0dfdcc3b65147f7dcb5387615
edward <edward@roojs.com>
2016-02-05 11:49:31 +0800

    Report/SendEventErrors.php

--- Report/SendEventErrors.php
+++ Report/SendEventErrors.php
@@ -41,7 +41,7 @@ class Pman_Admin_Report_SendEventErrors extends Pman_Roo
             $this->jerr('Missing group - try add [-t {group name}]');
         }
         
-        
+        $rcpts = DB_DataObject::factory('groups')->lookupMembers("{$this->opts->group}",'email');
         
         
         $this->jok("Done");
