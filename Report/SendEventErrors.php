38fd2b8d9b4df7055496b038b6582c792654162c
edward <edward@roojs.com>
2016-02-05 11:40:41 +0800

    Report/SendEventErrors.php

--- Report/SendEventErrors.php
+++ Report/SendEventErrors.php
@@ -29,41 +29,7 @@ class Pman_Admin_Report_SendEventErrors extends Pman_Roo
         
         PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, array($this, 'onPearError'));
         
-        $hydra_person = DB_DataObject::factory('hydra_person');
-        $hydra_person->company_processed = 0;
-        $cols = $hydra_person->table();
-        
-        if(!isset($cols['company_processed'])){
-            $this->jerr('Migration has been completed. Do not need to run again');
-        }
-        
-        foreach ($hydra_person->fetchAll() as $hydra_person){
-            
-            $core_enum = DB_DataObject::factory('core_enum');
-            
-            if(
-                    empty($hydra_person->employer_id) || 
-                    !empty($hydra_person->company_processed) ||
-                    !$core_enum->get($hydra_person->employer_id)
-            ){
-                continue;
-            }
-            
-            $company = DB_DataObject::factory('hydra_company');
-            $company->whereAdd("BINARY name = '{$company->escape($core_enum->name)}'");
-            
-            if(!$company->find(true)){
-                $company = DB_DataObject::factory('hydra_company');
-                $company->name = $core_enum->name;
-                $company->insert();
-            }
-            
-            $o = clone($hydra_person);
-            $hydra_person->employer_id = $company->id;
-            $hydra_person->company_processed = 1;
-            $hydra_person->update($o);
-            
-        }
+        print_r('run');exit;
         
         $this->jok("Done");
         
