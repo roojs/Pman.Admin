2fe759bf6a203262bd52a294510b87a9d0499bba
Alan Knowles <alan@roojs.com>
2016-04-11 14:36:40 +0800

    Pman.Tab.AdminWatchNotify.bjs Pman.Tab.AdminWatchNotify.js

--- Pman.Tab.AdminWatchNotify.js
+++ Pman.Tab.AdminWatchNotify.js
@@ -2,33 +2,42 @@
 
 // Auto generated file - created by app.Builder.js- do not edit directly (at present!)
 
+Roo.namespace('Pman.Tab');
+
 Pman.Tab.AdminWatchNotify = new Roo.XComponent({
-    part     :  ["Admin","WatchNotify"],
-    order    : '860-Pman.Tab.AdminWatchNotify',
-    region   : 'center',
-    parent   : 'Pman.Tab.Admin',
-    name     : "Notifications",
-    disabled : false, 
-    permname : '', 
-    _tree : function()
-    {
-        var _this = this;
-        var MODULE = this;
-        return {
-            xtype: 'NestedLayoutPanel',
-            xns: Roo,
-            background : true,
-            region : 'center',
-            title : "Notifications",
-            layout : {
-                xtype: 'BorderLayout',
-                xns: Roo,
-                center : {
-                    xtype: 'LayoutRegion',
-                    xns: Roo,
-                    tabPosition : 'top'
-                }
-            }
-        };
+
+ _strings : {
+  'a274f4d4670213a9045ce258c6c56b80' :"Notifications"
+ },
+
+  part     :  ["Admin", "WatchNotify" ],
+  order    : '860-Pman.Tab.AdminWatchNotify',
+  region   : 'center',
+  parent   : 'Pman.Tab.Admin',
+  name     : "Notifications",
+  disabled : false, 
+  permname : '', 
+  _tree : function()
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   background : false,
+   region : 'center',
+   title : _this._strings['a274f4d4670213a9045ce258c6c56b80'] /* Notifications */,
+   xns : Roo,
+   '|xns' : 'Roo',
+   xtype : 'NestedLayoutPanel',
+   layout : {
+    xns : Roo,
+    '|xns' : 'Roo',
+    xtype : 'BorderLayout',
+    center : {
+     tabPosition : 'top',
+     xns : Roo,
+     '|xns' : 'Roo',
+     xtype : 'LayoutRegion'
     }
+   }
+  };  }
 });
