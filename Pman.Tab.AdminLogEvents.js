84b2a37e368025b134d3d21b419547254bfe41fd
edward <edward@roojs.com>
2016-07-22 16:40:58 +0800

    Pman.Tab.AdminLogEvents.bjs Pman.Tab.AdminLogEvents.js

--- Pman.Tab.AdminLogEvents.js
+++ Pman.Tab.AdminLogEvents.js
@@ -1354,12 +1354,7 @@ Pman.Tab.AdminLogEvents = new Roo.XComponent({
          dataIndex : 'id',
          header : _this._strings['1b7f7d24575cdbfadb83b9587fa1bf97'] /* #id */,
          hidden : (function() {
-         
-             Roo.log('run???');
-            Roo.log(this);
-            
-            return false;
-         
+             return typeof(Pman.Tab.AdminLogEvents.hide_cols) == undefined ? false : Pman.Tab.AdminLogEvents.hide_cols.indexOf('id') > -1
          })(),
          width : 100,
          xns : Roo.grid,
