59be9ce454ac201e4e8939b5513d684349dd9a08
edward <edward@roojs.com>
2016-08-19 14:20:22 +0800

    Pman.Tab.PersonStaff.js

--- Pman.Tab.PersonStaff.js
+++ Pman.Tab.PersonStaff.js
@@ -39,7 +39,6 @@ Pman.Tab.PersonStaff = new  Pman.Tab.PersonList({
     beforeload: function(t, o) {
         //console.log(o.params);
         // teams!?!
-        Roo.log('beforeLoad?????');
         var tms = this.getLeftSelections();
         
         if (tms.length) {
