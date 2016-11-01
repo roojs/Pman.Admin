df4e085f9522a119a28273dba909e070cd462488
edward <edward@roojs.com>
2016-11-01 15:41:15 +0800

    Pman.Tab.AdminLogs.bjs Pman.Tab.AdminLogs.js

--- Pman.Tab.AdminLogs.js
+++ Pman.Tab.AdminLogs.js
@@ -31,6 +31,7 @@ Pman.Tab.AdminLogs = new Roo.XComponent({
     activate : function (_self)
      {
          var cr = this.layout.getRegion('center');
+         Roo.log(cr);
          if (cr) {
              cr.showPanel(cr.activePanel);
          }
