6121b6e093b3b143197f617e03c4c824ae4895f0
edward <edward@roojs.com>
2016-11-04 13:10:24 +0800

    Pman.Dialog.AdminCompanyMerge.bjs Pman.Dialog.AdminCompanyMerge.js

---
+++ Pman.Dialog.AdminCompanyMerge.js
@@ -0,0 +1,218 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Roo.namespace('Pman.Dialog');
+
+Pman.Dialog.AdminCompanyMerge = {
+
+ _strings : {
+  'bf8691517ce00a09186a05cd65863091' :"Select Item to Merge With",
+  '298a183cfe4fddedd4bd17abe8aeb685' :"Merge Pulldown Option",
+  '03e956f1dca2b4d525df03cb1899cb6f' :"Merge with",
+  '1243daf593fa297e07ab03bf06d925af' :"Searching...",
+  '68be4837f6c739877233e527a996dd00' :"Merge",
+  'ea4788705e6873b424c65e91c2846b19' :"Cancel"
+ },
+
+ dialog : false,
+ callback:  false,
+
+ show : function(data, cb)
+ {
+  if (!this.dialog) {
+   this.create();
+  }
+
+  this.callback = cb;
+  this.data = data;
+  this.dialog.show(this.data._el);
+  if (this.form) {
+   this.form.reset();
+   this.form.setValues(data);
+   this.form.fireEvent('actioncomplete', this.form,  { type: 'setdata', data: data });
+  }
+
+ },
+
+ create : function()
+ {
+   var _this = this;
+   this.dialog = Roo.factory({
+    xtype : 'LayoutDialog',
+    closable : false,
+    collapsible : false,
+    height : 120,
+    modal : true,
+    resizable : false,
+    title : _this._strings['298a183cfe4fddedd4bd17abe8aeb685'] /* Merge Pulldown Option */,
+    width : 400,
+    listeners : {
+     show : function (_self)
+      {
+          if (_this.isBuilder) {
+              _this.data = { id : 2, comptype : 'SUPPLIER' }
+          }
+      }
+    },
+    xns : Roo,
+    '|xns' : 'Roo',
+    center : {
+     xtype : 'LayoutRegion',
+     xns : Roo,
+     '|xns' : 'Roo'
+    },
+    buttons : [
+     {
+      xtype : 'Button',
+      text : _this._strings['ea4788705e6873b424c65e91c2846b19'] /* Cancel */,
+      listeners : {
+       click : function (_self, e)
+        {
+            _this.dialog.hide();
+        }
+      },
+      xns : Roo,
+      '|xns' : 'Roo'
+     },
+     {
+      xtype : 'Button',
+      text : _this._strings['68be4837f6c739877233e527a996dd00'] /* Merge */,
+      listeners : {
+       click : function (_self, e)
+        {
+             
+            _this.form.doAction("submit");
+        
+        }
+      },
+      xns : Roo,
+      '|xns' : 'Roo'
+     }
+    ],
+    items  : [
+     {
+      xtype : 'ContentPanel',
+      region : 'center',
+      xns : Roo,
+      '|xns' : 'Roo',
+      items  : [
+       {
+        xtype : 'Form',
+        method : 'POST',
+        style : 'margin: 10px;',
+        url : baseURL + '/Roo/Core_enum.php',
+        listeners : {
+         actioncomplete : function (_self, action)
+          {
+          
+             if (action.type =='submit') {
+                 
+                   _this.dialog.hide();
+                 
+                  if (_this.callback) {
+                     _this.callback.call(_this, _this.form.getValues());
+                  }
+                  _this.form.reset();
+                  return;
+              }
+              if (action.type == 'setdata') {
+                  
+                   var title = _this.data.title  || _this.data.etype;
+                  _this.dialog.setTitle("Delete selected " + title + " and merge data with");
+                   
+                 return;
+              }
+              
+          },
+         actionfailed : function (_self, action)
+          {
+           
+              Pman.standardActionFailed(_self, action);
+          },
+         rendered : function (form)
+          {
+             _this.form = form;
+          }
+        },
+        xns : Roo.form,
+        '|xns' : 'Roo.form',
+        items  : [
+         {
+          xtype : 'ComboBox',
+          allowBlank : false,
+          alwaysQuery : true,
+          displayField : 'name',
+          emptyText : _this._strings['bf8691517ce00a09186a05cd65863091'] /* Select Item to Merge With */,
+          fieldLabel : _this._strings['03e956f1dca2b4d525df03cb1899cb6f'] /* Merge with */,
+          forceSelection : true,
+          hiddenName : '_merge_id',
+          listWidth : 400,
+          loadingText : _this._strings['1243daf593fa297e07ab03bf06d925af'] /* Searching... */,
+          minChars : 2,
+          name : '_merge_id_name',
+          pageSize : 20,
+          qtip : _this._strings['bf8691517ce00a09186a05cd65863091'] /* Select Item to Merge With */,
+          queryParam : 'query[search_begins]',
+          selectOnFocus : true,
+          tpl : '<div class=\"x-grid-cell-text x-btn button\"><b>{display_name}</b> {name}</div>',
+          triggerAction : 'all',
+          typeAhead : true,
+          valueField : 'id',
+          width : 250,
+          xns : Roo.form,
+          '|xns' : 'Roo.form',
+          store : {
+           xtype : 'Store',
+           remoteSort : true,
+           sortInfo : { direction : 'ASC', field: 'name' },
+           listeners : {
+            beforeload : function (_self, o){
+                 o.params = o.params || {};
+                 
+                 o.params['etype'] = _this.data.etype;
+                 
+                 o.params['!id'] = _this.form.findField('id').getValue();
+                 // set more here
+             }
+           },
+           xns : Roo.data,
+           '|xns' : 'Roo.data',
+           proxy : {
+            xtype : 'HttpProxy',
+            method : 'GET',
+            url : baseURL + '/Roo/Core_enum',
+            xns : Roo.data,
+            '|xns' : 'Roo.data'
+           },
+           reader : {
+            xtype : 'JsonReader',
+            fields : [{"name":"id","type":"int"},{"name":"name","type":"string"}],
+            id : 'id',
+            root : 'data',
+            totalProperty : 'total',
+            xns : Roo.data,
+            '|xns' : 'Roo.data'
+           }
+          }
+         },
+         {
+          xtype : 'Hidden',
+          name : 'etype',
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
+         },
+         {
+          xtype : 'Hidden',
+          name : 'id',
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
+         }
+        ]
+       }
+      ]
+     }
+    ]
+   });
+ }
+};
