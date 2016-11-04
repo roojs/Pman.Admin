47b00f49c3575b159db8ddf7e67318f336fcf633
edward <edward@roojs.com>
2016-11-04 13:09:45 +0800

    Pman.Tab.AdminCompanies.bjs Pman.Tab.AdminCompanies.js

--- Pman.Tab.AdminCompanies.js
+++ Pman.Tab.AdminCompanies.js
@@ -190,9 +190,6 @@ Pman.Tab.AdminCompanies = new Roo.XComponent({
                     return;
                 }
                 
-                Roo.log(s);
-                return;
-                
                 Pman.Dialog.AdminCompanyMerge.show(s[0].data, function() {
                     _this.grid.footer.onClick('first');
                 }); 
