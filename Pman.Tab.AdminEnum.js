6744d12dd2736f1e531dc7799f829cc755713e78
edward <edward@roojs.com>
2016-11-04 13:10:14 +0800

    Pman.Tab.AdminCompanies.bjs Pman.Tab.AdminCompanies.js Pman.Dialog.AdminCompanyMerge.bjs Pman.Tab.AdminEnum.bjs Pman.Tab.AdminEnum.js

--- Pman.Tab.AdminEnum.js
+++ Pman.Tab.AdminEnum.js
@@ -25,7 +25,7 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
   '4d3d769b812b6faa6b76e1a8abaece2d' :"Active",
   'f2a6c498fb90ee345d997f888fce3b18' :"Delete",
   '49ee3087348e8d44e1feda1917443987' :"Name",
-  '7215ee9c7d9dc229d2921a40e899ec5f' :" "
+  'd41d8cd98f00b204e9800998ecf8427e' :" "
  },
 
   part     :  ["Admin", "Enum" ],
@@ -40,36 +40,34 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
    var _this = this;
    var MODULE = this;
    return {
+   xtype : 'NestedLayoutPanel',
    region : 'center',
    title : _this._strings['2df80d5febcde0c10a66818488622b7c'] /* Pulldown Options */,
    xns : Roo,
    '|xns' : 'Roo',
-   xtype : 'NestedLayoutPanel',
    layout : {
+    xtype : 'BorderLayout',
     xns : Roo,
     '|xns' : 'Roo',
-    xtype : 'BorderLayout',
     center : {
+     xtype : 'LayoutRegion',
      xns : Roo,
-     '|xns' : 'Roo',
-     xtype : 'LayoutRegion'
+     '|xns' : 'Roo'
     },
     west : {
+     xtype : 'LayoutRegion',
      split : true,
      width : 200,
      xns : Roo,
-     '|xns' : 'Roo',
-     xtype : 'LayoutRegion'
+     '|xns' : 'Roo'
     },
     items  : [
      {
+      xtype : 'GridPanel',
       background : false,
       region : 'west',
       tableName : 'core_enum',
       title : _this._strings['2df80d5febcde0c10a66818488622b7c'] /* Pulldown Options */,
-      xns : Roo,
-      '|xns' : 'Roo',
-      xtype : 'GridPanel',
       listeners : {
        activate : function() {
             _this.epanel = this;
@@ -78,13 +76,13 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
             //}
         }
       },
+      xns : Roo,
+      '|xns' : 'Roo',
       grid : {
+       xtype : 'EditorGrid',
        autoExpandColumn : 'display_name',
        clicksToEdit : 1,
        loadMask : true,
-       xns : Roo.grid,
-       '|xns' : 'Roo.grid',
-       xtype : 'EditorGrid',
        listeners : {
         afteredit : function (e)
          {
@@ -128,27 +126,27 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
              }
          }
        },
+       xns : Roo.grid,
+       '|xns' : 'Roo.grid',
        footer : {
+        xtype : 'PagingToolbar',
         displayInfo : false,
-        displayMsg : _this._strings['7215ee9c7d9dc229d2921a40e899ec5f'] /*   */,
+        displayMsg : _this._strings['d41d8cd98f00b204e9800998ecf8427e'] /*   */,
         emptyMsg : _this._strings['1ba4d808fc7b27a7f60ce2ff75a8af3a'] /* No core_enum found */,
         pageSize : 100,
         xns : Roo,
-        '|xns' : 'Roo',
-        xtype : 'PagingToolbar'
+        '|xns' : 'Roo'
        },
        toolbar : {
+        xtype : 'Toolbar',
         xns : Roo,
         '|xns' : 'Roo',
-        xtype : 'Toolbar',
         items  : [
          {
+          xtype : 'Button',
           cls : 'x-btn-text-icon',
           icon : Roo.rootURL + 'images/default/dd/drop-add.gif',
           text : _this._strings['fdff10eac021dfbb69e9c38204237fdc'] /* Add new pulldown list */,
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Button',
           listeners : {
            click : function()
             {
@@ -167,16 +165,16 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                      
             
             }
-          }
+          },
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
          }
         ]
        },
        dataSource : {
+        xtype : 'Store',
         remoteSort : true,
         sortInfo : { field : 'display_name', direction: 'ASC' },
-        xns : Roo.data,
-        '|xns' : 'Roo.data',
-        xtype : 'Store',
         listeners : {
          beforeload : function (_self, o)
           {
@@ -211,14 +209,17 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
               
           }
         },
+        xns : Roo.data,
+        '|xns' : 'Roo.data',
         proxy : {
+         xtype : 'HttpProxy',
          method : 'GET',
          url : baseURL + '/Roo/core_enum.php',
          xns : Roo.data,
-         '|xns' : 'Roo.data',
-         xtype : 'HttpProxy'
+         '|xns' : 'Roo.data'
         },
         reader : {
+         xtype : 'JsonReader',
          fields : [
              {
                  'name': 'id',
@@ -245,23 +246,23 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          root : 'data',
          totalProperty : 'total',
          xns : Roo.data,
-         '|xns' : 'Roo.data',
-         xtype : 'JsonReader'
+         '|xns' : 'Roo.data'
         }
        },
        sm : {
-        xns : Roo.grid,
-        '|xns' : 'Roo.grid',
         xtype : 'CellSelectionModel',
         listeners : {
          selectionchange : function (_self, selection)
           {
               _this.grid.footer.onClick('first');
           }
-        }
+        },
+        xns : Roo.grid,
+        '|xns' : 'Roo.grid'
        },
        colModel : [
         {
+         xtype : 'ColumnModel',
          dataIndex : 'display_name',
          header : _this._strings['1206eb8aea05be4625371c9c12818785'] /* Pulldown */,
          renderer : function(v,x,r) { 
@@ -271,10 +272,10 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          },
          width : 200,
          xns : Roo.grid,
-         '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel'
+         '|xns' : 'Roo.grid'
         },
         {
+         xtype : 'ColumnModel',
          dataIndex : 'active',
          header : _this._strings['4d3d769b812b6faa6b76e1a8abaece2d'] /* Active */,
          renderer : function(v) {  
@@ -285,21 +286,18 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
           },
          width : 50,
          xns : Roo.grid,
-         '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel'
+         '|xns' : 'Roo.grid'
         }
        ]
       }
      },
      {
+      xtype : 'GridPanel',
       background : false,
       fitToframe : true,
       region : 'center',
       tableName : 'core_enum',
       title : _this._strings['2df80d5febcde0c10a66818488622b7c'] /* Pulldown Options */,
-      xns : Roo,
-      '|xns' : 'Roo',
-      xtype : 'GridPanel',
       listeners : {
        activate : function() {
             _this.panel = this;
@@ -308,13 +306,13 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
             }
         }
       },
+      xns : Roo,
+      '|xns' : 'Roo',
       grid : {
+       xtype : 'EditorGrid',
        autoExpandColumn : 'display_name',
        clicksToEdit : 1,
        loadMask : true,
-       xns : Roo.grid,
-       '|xns' : 'Roo.grid',
-       xtype : 'EditorGrid',
        listeners : {
         afteredit : function (e)
          {
@@ -388,20 +386,20 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
              }
          }
        },
+       xns : Roo.grid,
+       '|xns' : 'Roo.grid',
        footer : {
+        xtype : 'PagingToolbar',
         displayInfo : true,
         displayMsg : _this._strings['510bc6e58593b2b8002c9fe0c21f3fde'] /* Displaying core_enum{0} - {1} of {2} */,
         emptyMsg : _this._strings['1ba4d808fc7b27a7f60ce2ff75a8af3a'] /* No core_enum found */,
         pageSize : 25,
         xns : Roo,
         '|xns' : 'Roo',
-        xtype : 'PagingToolbar',
         items  : [
          {
-          text : _this._strings['801ab24683a4a8c433c6eb40c48bcd9d'] /* Download */,
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
           xtype : 'Button',
+          text : _this._strings['801ab24683a4a8c433c6eb40c48bcd9d'] /* Download */,
           listeners : {
            click : function (_self, e)
             {
@@ -410,13 +408,13 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                 });
                 Roo.MessageBox.alert("Downloading", "File is downloading");
             }
-          }
+          },
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
          },
          {
-          text : _this._strings['c550aeed26e71a81a88360c1889245ab'] /* Upload Values */,
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
           xtype : 'Button',
+          text : _this._strings['c550aeed26e71a81a88360c1889245ab'] /* Upload Values */,
           listeners : {
            click : function (_self, e)
             {
@@ -448,22 +446,22 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                    }
                );
             }
-          }
+          },
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
          }
         ]
        },
        toolbar : {
+        xtype : 'Toolbar',
         xns : Roo,
         '|xns' : 'Roo',
-        xtype : 'Toolbar',
         items  : [
          {
+          xtype : 'Button',
           cls : 'x-btn-text-icon',
           icon : rootURL + '/Pman/templates/images/search.gif',
           text : _this._strings['d9ec74f5aa29ceef6bf7b45f7fec5d0f'] /* Add Value */,
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Button',
           listeners : {
            click : function()
             {
@@ -496,15 +494,15 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                 
                 _this.grid.startEditing(0, _this.grid.colModel.findColumnIndex('name')); // name... 
             }
-          }
+          },
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
          },
          {
+          xtype : 'Button',
           cls : 'x-btn-text-icon',
           icon : Roo.rootURL + 'images/default/dd/drop-add.gif',
           text : _this._strings['b9c49611cfda3259a2b837b39489e650'] /* Add Image */,
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Button',
           listeners : {
            click : function()
             {
@@ -530,60 +528,60 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                 });
                 
             }
-          }
+          },
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
          },
          {
+          xtype : 'TextField',
           fieldLabel : _this._strings['13348442cc6a27032d2b4aa28b75a5d3'] /* Search */,
           name : 'search',
-          xns : Roo.form,
-          '|xns' : 'Roo.form',
-          xtype : 'TextField',
           listeners : {
            render : function (_self)
             {
                 _this.searchBox = this;
             }
-          }
+          },
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
          },
          {
+          xtype : 'Button',
           cls : 'x-btn-icon',
           icon : rootURL + '/Pman/templates/images/search.gif',
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Button',
           listeners : {
            click : function (_self, e)
             {
                 _this.grid.footer.onClick('first');
             }
-          }
+          },
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
          },
          {
+          xtype : 'Button',
           cls : 'x-btn-icon',
           icon : rootURL + '/Pman/templates/images/edit-clear.gif',
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Button',
           listeners : {
            click : function (_self, e)
             {
                  _this.searchBox.setValue('');
                  _this.grid.footer.onClick('first');
             }
-          }
-         },
-         {
+          },
           xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Fill'
+          '|xns' : 'Roo.Toolbar'
          },
          {
+          xtype : 'Fill',
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
+         },
+         {
+          xtype : 'Button',
           cls : 'x-btn-text-icon',
           icon : rootURL + '/Pman/templates/images/trash.gif',
           text : _this._strings['f2a6c498fb90ee345d997f888fce3b18'] /* Delete */,
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Button',
           listeners : {
            click : function()
             {
@@ -620,20 +618,20 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                 
                 
             }
-          }
-         },
-         {
+          },
           xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Separator'
+          '|xns' : 'Roo.Toolbar'
          },
          {
+          xtype : 'Separator',
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
+         },
+         {
+          xtype : 'Button',
           cls : 'x-btn-text-icon',
           icon : Roo.rootURL + 'images/default/tree/leaf.gif',
           text : _this._strings['1a87a3d29199e3435b8fb314624c008f'] /* Merge Option */,
-          xns : Roo.Toolbar,
-          '|xns' : 'Roo.Toolbar',
-          xtype : 'Button',
           listeners : {
            click : function()
             {
@@ -651,16 +649,16 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                 }); 
                 
             }
-          }
+          },
+          xns : Roo.Toolbar,
+          '|xns' : 'Roo.Toolbar'
          }
         ]
        },
        dataSource : {
+        xtype : 'Store',
         remoteSort : true,
         sortInfo : { field : 'etype', direction: 'ASC' },
-        xns : Roo.data,
-        '|xns' : 'Roo.data',
-        xtype : 'Store',
         listeners : {
          beforeload : function (_self, options)
           {
@@ -707,14 +705,17 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
               
           }
         },
+        xns : Roo.data,
+        '|xns' : 'Roo.data',
         proxy : {
+         xtype : 'HttpProxy',
          method : 'GET',
          url : baseURL + '/Roo/core_enum.php',
          xns : Roo.data,
-         '|xns' : 'Roo.data',
-         xtype : 'HttpProxy'
+         '|xns' : 'Roo.data'
         },
         reader : {
+         xtype : 'JsonReader',
          fields : [
              {
                  'name': 'id',
@@ -741,12 +742,12 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          root : 'data',
          totalProperty : 'total',
          xns : Roo.data,
-         '|xns' : 'Roo.data',
-         xtype : 'JsonReader'
+         '|xns' : 'Roo.data'
         }
        },
        colModel : [
         {
+         xtype : 'ColumnModel',
          dataIndex : 'id',
          header : _this._strings['7af54708cf5a4286cf0cfa58ff5148a8'] /* Internal # */,
          renderer : function(v,x,r) { 
@@ -758,19 +759,19 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          sortable : true,
          width : 75,
          xns : Roo.grid,
-         '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel'
+         '|xns' : 'Roo.grid'
         },
         {
+         xtype : 'ColumnModel',
          dataIndex : 'images_id_id',
          header : _this._strings['be53a0541a6d36f6ecb879fa2c584b08'] /* Image */,
          renderer : function(v,x,r) { return String.format('<img src="{0}/Images/Thumb/25/{1}/{2}" width="25" height="25">', baseURL, v, r.data.images_id_filename); },
          width : 75,
          xns : Roo.grid,
-         '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel'
+         '|xns' : 'Roo.grid'
         },
         {
+         xtype : 'ColumnModel',
          dataIndex : 'name',
          header : _this._strings['49ee3087348e8d44e1feda1917443987'] /* Name */,
          renderer : function(v) { return String.format('{0}', v); },
@@ -778,19 +779,19 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          width : 200,
          xns : Roo.grid,
          '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel',
          editor : {
+          xtype : 'GridEditor',
           xns : Roo.grid,
           '|xns' : 'Roo.grid',
-          xtype : 'GridEditor',
           field : {
+           xtype : 'TextField',
            xns : Roo.form,
-           '|xns' : 'Roo.form',
-           xtype : 'TextField'
+           '|xns' : 'Roo.form'
           }
          }
         },
         {
+         xtype : 'ColumnModel',
          dataIndex : 'display_name',
          header : _this._strings['b48968e1c912da07df5e8d6d246291ec'] /* Display Name */,
          renderer : function(v) { return String.format('{0}', v); },
@@ -798,19 +799,19 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          width : 200,
          xns : Roo.grid,
          '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel',
          editor : {
+          xtype : 'GridEditor',
           xns : Roo.grid,
           '|xns' : 'Roo.grid',
-          xtype : 'GridEditor',
           field : {
+           xtype : 'TextField',
            xns : Roo.form,
-           '|xns' : 'Roo.form',
-           xtype : 'TextField'
+           '|xns' : 'Roo.form'
           }
          }
         },
         {
+         xtype : 'ColumnModel',
          dataIndex : 'active',
          header : _this._strings['4d3d769b812b6faa6b76e1a8abaece2d'] /* Active */,
          renderer : function(v) {  
@@ -822,10 +823,10 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          sortable : true,
          width : 75,
          xns : Roo.grid,
-         '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel'
+         '|xns' : 'Roo.grid'
         },
         {
+         xtype : 'ColumnModel',
          dataIndex : 'seqid',
          header : _this._strings['d1228f5476d15142b1358ae4b5fa2454'] /* Order # */,
          renderer : function(v) { return String.format('{0}', v); },
@@ -833,18 +834,17 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
          width : 75,
          xns : Roo.grid,
          '|xns' : 'Roo.grid',
-         xtype : 'ColumnModel',
          editor : {
+          xtype : 'GridEditor',
           xns : Roo.grid,
           '|xns' : 'Roo.grid',
-          xtype : 'GridEditor',
           field : {
+           xtype : 'NumberField',
            allowDecimals : false,
            allowNegative : true,
            decimalPrecision : 0,
            xns : Roo.form,
-           '|xns' : 'Roo.form',
-           xtype : 'NumberField'
+           '|xns' : 'Roo.form'
           }
          }
         }
