0190c2d043d528a3409ad38026e034ab6a0c3a8f
Alan Knowles <alan@roojs.com>
2017-08-04 09:51:13 +0800

    EventView.php

--- EventView.php
+++ EventView.php
@@ -18,7 +18,7 @@ class Pman_Admin_EventView extends Pman
         
     }
     
-    function get($id)
+    function get($id, $opts = Array))
     {   
         $ev = DB_DataObject::Factory('Events');
         if (!$ev->get((int)$id)) {
