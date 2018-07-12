9c63d4af681be7624935abc09189940df7e585c9
benny <benny@roojs.com>
2018-07-12 12:47:18 +0800

    Pman.Dialog.AdminEnumImages.bjs Pman.Dialog.AdminEnumImages.js

--- Pman.Dialog.AdminEnumImages.js
+++ Pman.Dialog.AdminEnumImages.js
@@ -174,7 +174,7 @@ Pman.Dialog.AdminEnumImages = {
                  }
                  var data = _this.grid.getDataSource().getById(s[0].data.id).data;
                 
-                         Pman.Dialog.PressReleaseImage.show(data, function() {
+                         Pman.Dialog.Image.show(data, function() {
                             _this.grid.footer.onClick('first');
                            });
                         
