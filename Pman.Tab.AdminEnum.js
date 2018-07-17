55c9f4fc59790a9f81ea81fed1d096b7b586cecf
benny <benny@roojs.com>
2018-07-17 15:51:13 +0800

    Pman.Tab.AdminEnum.bjs Pman.Tab.AdminEnum.js

--- Pman.Tab.AdminEnum.js
+++ Pman.Tab.AdminEnum.js
@@ -647,7 +647,7 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
           listeners : {
            click : function()
             {
-                console.log(_this.grid.selModel);
+                console.log(_this.grid.selModel.getSelectedCell);
                 var s = _this.grid.getSelectionModel().getSelections();
                 
                 if (!s.length)  {
