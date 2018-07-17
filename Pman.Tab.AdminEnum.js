f746d1f7f8513810aff0e690b38ecc90cdc3169d
benny <benny@roojs.com>
2018-07-17 16:19:30 +0800

    Pman.Tab.AdminEnum.bjs Pman.Tab.AdminEnum.js

--- Pman.Tab.AdminEnum.js
+++ Pman.Tab.AdminEnum.js
@@ -654,7 +654,7 @@ Pman.Tab.AdminEnum = new Roo.XComponent({
                     Roo.MessageBox.alert("Error", "Select a Row");
                     return;
                 }
-                
+                console.log(_this.grid.ds);
                 var d = _this.grid.ds.getAt(s[0]);
             
                 Pman.Dialog.AdminEnumMerge.show(d.data, function() {
