5b031272d6684c30c6e8cc294a3000fde67f5a3b
Edward <edward@roojs.com>
2019-02-18 15:18:34 +0800

    Pman.Dialog.AdminGeoDivisionEdit.bjs Pman.Dialog.AdminGeoDivisionEdit.js

--- Pman.Dialog.AdminGeoDivisionEdit.js
+++ Pman.Dialog.AdminGeoDivisionEdit.js
@@ -7,7 +7,10 @@ Roo.namespace('Pman.Dialog');
 Pman.Dialog.AdminGeoDivisionEdit = {
 
  _strings : {
+  '1243daf593fa297e07ab03bf06d925af' :"Searching...",
+  'ce26601dac0dea138b7295f02b7620a7' :"Customer",
   'ea4788705e6873b424c65e91c2846b19' :"Cancel",
+  '9675747b5ab12d05f18518761e68a533' :"Select Companies",
   'd469fd7a8d8939b7abe793469d7375da' :"Add / Edit Province",
   '49ee3087348e8d44e1feda1917443987' :"Name",
   'ca0dbad92a874b2f69b549293387925e' :"Code",
@@ -15,7 +18,11 @@ Pman.Dialog.AdminGeoDivisionEdit = {
  },
  _named_strings : {
   'code_fieldLabel' : 'ca0dbad92a874b2f69b549293387925e' /* Code */ ,
-  'name_fieldLabel' : '49ee3087348e8d44e1feda1917443987' /* Name */ 
+  'company_customer_id_name_qtip' : '9675747b5ab12d05f18518761e68a533' /* Select Companies */ ,
+  'name_fieldLabel' : '49ee3087348e8d44e1feda1917443987' /* Name */ ,
+  'company_customer_id_name_emptyText' : '9675747b5ab12d05f18518761e68a533' /* Select Companies */ ,
+  'company_customer_id_name_loadingText' : '1243daf593fa297e07ab03bf06d925af' /* Searching... */ ,
+  'company_customer_id_name_fieldLabel' : 'ce26601dac0dea138b7295f02b7620a7' /* Customer */ 
  },
 
  dialog : false,
@@ -141,6 +148,7 @@ Pman.Dialog.AdminGeoDivisionEdit = {
         items  : [
          {
           xtype : 'TextField',
+          allowBlank : false,
           fieldLabel : _this._strings['ca0dbad92a874b2f69b549293387925e'] /* Code */,
           name : 'code',
           width : 200,
@@ -149,11 +157,68 @@ Pman.Dialog.AdminGeoDivisionEdit = {
          },
          {
           xtype : 'TextField',
+          allowBlank : false,
           fieldLabel : _this._strings['49ee3087348e8d44e1feda1917443987'] /* Name */,
           name : 'name',
           width : 200,
           xns : Roo.form,
           '|xns' : 'Roo.form'
+         },
+         {
+          xtype : 'ComboBox',
+          allowBlank : false,
+          alwaysQuery : true,
+          displayField : 'name',
+          editable : false,
+          emptyText : _this._strings['9675747b5ab12d05f18518761e68a533'] /* Select Companies */,
+          fieldLabel : _this._strings['ce26601dac0dea138b7295f02b7620a7'] /* Customer */,
+          forceSelection : true,
+          hiddenName : 'company_customer_id',
+          listWidth : 400,
+          loadingText : _this._strings['1243daf593fa297e07ab03bf06d925af'] /* Searching... */,
+          minChars : 2,
+          name : 'company_customer_id_name',
+          pageSize : 20,
+          qtip : _this._strings['9675747b5ab12d05f18518761e68a533'] /* Select Companies */,
+          queryParam : 'query[code]',
+          selectOnFocus : true,
+          tpl : '<div class=\"x-grid-cell-text x-btn button\"><b>{name}</b> </div>',
+          triggerAction : 'all',
+          typeAhead : true,
+          valueField : 'id',
+          width : 300,
+          xns : Roo.form,
+          '|xns' : 'Roo.form',
+          store : {
+           xtype : 'Store',
+           remoteSort : true,
+           sortInfo : { direction : 'ASC', field: 'name' },
+           listeners : {
+            beforeload : function (_self, o){
+                 o.params = o.params || {};
+                 // set more here
+                 o.params.comptype= 'CUSTOMER';
+             }
+           },
+           xns : Roo.data,
+           '|xns' : 'Roo.data',
+           proxy : {
+            xtype : 'HttpProxy',
+            method : 'GET',
+            url : baseURL + '/Roo/core_company.php',
+            xns : Roo.data,
+            '|xns' : 'Roo.data'
+           },
+           reader : {
+            xtype : 'JsonReader',
+            fields : [{"name":"id","type":"int"},{"name":"code","type":"string"}],
+            id : 'id',
+            root : 'data',
+            totalProperty : 'total',
+            xns : Roo.data,
+            '|xns' : 'Roo.data'
+           }
+          }
          },
          {
           xtype : 'Hidden',
