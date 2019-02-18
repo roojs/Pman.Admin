6e3754c08f7952ba0ddf70a9b310f42f1738e07d
Edward <edward@roojs.com>
2019-02-18 16:01:46 +0800

    Pman.Dialog.AdminGeoCityEdit.bjs Pman.Dialog.AdminGeoCityEdit.js

--- Pman.Dialog.AdminGeoCityEdit.js
+++ Pman.Dialog.AdminGeoCityEdit.js
@@ -59,7 +59,7 @@ Pman.Dialog.AdminGeoCityEdit = {
     xtype : 'LayoutDialog',
     closable : false,
     collapsible : false,
-    height : 300,
+    height : 250,
     modal : true,
     resizable : false,
     title : _this._strings['43ccafccb532cc57c20bc05a36aad7b7'] /* Add / Edit City */,
