ef55cd52810d2ac93e79f49b5cc96b8b8cfb6b72
Edward <edward@roojs.com>
2019-02-18 14:11:11 +0800

    Pman.Tab.AdminGeoLocations.bjs Pman.Tab.AdminGeoLocations.js

--- Pman.Tab.AdminGeoLocations.js
+++ Pman.Tab.AdminGeoLocations.js
@@ -530,14 +530,16 @@ Pman.Tab.AdminGeoLocations = new Roo.XComponent({
         listeners : {
          beforeload : function (_self, options)
           {
-             if ( _this.layout) {
-                //_this.layout.getRegion('east').hide() ;
-                }
-              options.params['query[comptype]'] ='SUPPLIER,OLDSUPPL'; // base on pulldown..
-              options.params['query[name]'] = _this.searchBox.getValue();
-              if (_this.provinceCombo.getValue()) {
-                 options.params['province'] = _this.provinceCombo.getValue();
-             }
+              options.params = options.params || {};
+              
+              var country = _this.country_grid.getSelectionMode().getSelected();
+              
+              if(!country) {
+                  this.removeAll();
+                  return false;
+              }
+              
+              options.params.country = country.data.lkey;
           },
          load : function (_self, records, options)
           {
