7e5b941dd8c58dfa3a3762931c573d2c3015e264
Edward <edward@roojs.com>
2019-02-18 13:53:23 +0800

    Pman.Tab.AdminGeoLocations.bjs Pman.Tab.AdminGeoLocations.js

--- Pman.Tab.AdminGeoLocations.js
+++ Pman.Tab.AdminGeoLocations.js
@@ -606,29 +606,6 @@ Pman.Tab.AdminGeoLocations = new Roo.XComponent({
         listeners : {
          afterselectionchange : function (_self)
           {
-              if (!this.getSelections().length) {
-                  if (_this.form) { _this.form.reset(); }
-                  _this.panel.layout.getRegion('east').el.mask("Select Supplier");
-                  _this.panel.layout.getRegion('south').el.mask("Select Supplier");
-                  return;
-              }
-              
-              if (_this.form) {
-                 _this.form.setValues(
-                      _this.grid.getDataSource().getById(this.getSelected().data.id).data
-                  );
-              }
-          
-              _this.pgrid.footer.onClick('first'); // load people.
-              _this.panel.layout.getRegion('east').el.unmask();
-              _this.panel.layout.getRegion('south').el.unmask();
-              
-              _this.panel.layout.getRegion('east').showPanel(0);
-             
-              if(this.getSelections().length > 1) {
-                  _this.panel.layout.getRegion('east').el.mask("Unable to Update");
-                  _this.panel.layout.getRegion('south').el.mask("Unable to Update");
-              }
               
           }
         },
