99dc904cb9d3de0528b2996148806d542a461fd2
Edward <edward@roojs.com>
2019-02-18 16:16:50 +0800

    Pman.Tab.AdminGeoLocations.bjs Pman.Tab.AdminGeoLocations.js

--- Pman.Tab.AdminGeoLocations.js
+++ Pman.Tab.AdminGeoLocations.js
@@ -768,7 +768,6 @@ Pman.Tab.AdminGeoLocations = new Roo.XComponent({
             {
                 var s = _this.city_grid.getSelectionModel().getSelections();
                 
-                Roo.log(s);
                 if (!s.length)  {
                     Roo.MessageBox.alert("Error", "Select city to delete");
                     return;
