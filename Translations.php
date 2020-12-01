ed3f2247779f704fb0a53b87f93c680f5687a0b0
Alan Knowles <alan@roojs.com>
2020-12-01 15:24:00 +0800

    Translations.php

--- Translations.php
+++ Translations.php
@@ -190,7 +190,7 @@ class Pman_Admin_Translations extends Pman
                 $jd = json_decode(file_get_contents($bjs));
                 $this->original[str_replace('.bjs', '', basename($bjs)) ] = $jd->strings;
             }
-            
+            print_r($this->original) exit;
             file_put_contents($tfile, json_encode($this->original));
             
             
