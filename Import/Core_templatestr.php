93b9dba240bbb54d484f7621644893b1b11fa816
Alan Knowles <alan@roojs.com>
2020-12-03 16:41:24 +0800

    Import/Core_templatestr.php

--- Import/Core_templatestr.php
+++ Import/Core_templatestr.php
@@ -104,8 +104,15 @@ class Pman_Admin_Import_Core_templatestr extends Pman
         exit;
         $tr->whereAdd("join_template_id_id.template='{$tr->escape($r['template'])}'");
         $tr->whereAdd("join_template_id_id.view_name='{$tr->escape($r['module'])}'");
-        $tr->whereAdd("join_src_id_id.mdsum='{$tr->escape($r['module'])}'");
-  
+        $tr->whereAdd("join_src_id_id.mdsum='{$tr->escape($r['code'])}'");
+        $tr->lang = $r['language'];
+        if ($tr->find(true)) {
+            $tt = clone($tr);
+            $tt->txt = $r['translation'];
+            $tt->update($tr);
+            return 1;
+        }
+        return 0;
     }
     
     
