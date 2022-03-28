1dcbf9c3a6409d9869898933795c476794ff0f32
Alan <alan@roojs.com>
2022-03-28 10:24:04 +0800

    UpdateBjsTemplates.php

--- UpdateBjsTemplates.php
+++ UpdateBjsTemplates.php
@@ -259,7 +259,7 @@ class Pman_Admin_UpdateBjsTemplates extends Pman
                 'skipdir' => array('images','css','js'),
                 
             ));
-            
+            print_r($ar);
             
             foreach($ar as $pg) {
                  
