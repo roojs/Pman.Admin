66f372c0407015d8483aff3eeef1261d6f4ef960
leon <leon@roojs.com>
2023-08-03 11:44:56 +0800

    UpdateBjsTemplates.php

--- UpdateBjsTemplates.php
+++ UpdateBjsTemplates.php
@@ -222,12 +222,7 @@ class Pman_Admin_UpdateBjsTemplates extends Pman
             }
             
             foreach($ff->Pman_Core['DataObjects_Core_templatestr']['tables'] as $table=>$cols){
-                $tableName = $table;
-                if(strpos($table, ',') !== false) {
-                    $ar = explode(',', $table);
-                    $tableName = $ar[0];
-                }
-                $t = DB_DataObject::factory($tableName);
+                $t = DB_DataObject::factory($table);
                 foreach($t->fetchAll() as $d) {
                     $cts->onTableChange($this, $d, 'update');
                 }
