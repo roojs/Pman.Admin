58b258844148c2d02d3e140575fe75b105df30f8
leon <leon@roojs.com>
2024-11-19 10:15:42 +0800

    Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.js

--- Pman.Dialog.AdminWatch.js
+++ Pman.Dialog.AdminWatch.js
@@ -415,7 +415,8 @@ Pman.Dialog.AdminWatch = {
                 .then((res) => res.json())
                 .then((json) => {
                     Roo.each(json, m => {
-                        _self.store.add(new Roo.data.Record({val: m.split(':')[1]}));
+                        arr = m.split(':');
+                        _self.store.add(new Roo.data.Record({table: arr[0], action: arr[1]}));
                     });
                 });
             },
