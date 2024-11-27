dba39a1ef8fbc483da2a4d4f555cfd37ccdb4028
Alan <alan@roojs.com>
2024-11-27 13:50:35 +0800

    fix #8329 - post crm report - edit watch  changes

--- Pman.Tab.AdminWatches.js
+++ Pman.Tab.AdminWatches.js
@@ -23,6 +23,11 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
   '4d3d769b812b6faa6b76e1a8abaece2d' :"Active",
   '314b26dabb519a609db698728284683f' :"Select Table"
  },
+ _named_strings : {
+  'ontable_qtip' : 'a8929eb5c1553d3f70497f862d25d0ce' /* Select Action */ ,
+  'ontable_loadingText' : '1243daf593fa297e07ab03bf06d925af' /* Searching... */ ,
+  'ontable_emptyText' : '314b26dabb519a609db698728284683f' /* Select Table */ 
+ },
 
   part     :  ["Admin", "Watches" ],
   order    : '860-Pman.Tab.AdminWatches',
@@ -31,11 +36,12 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
   name     : "Pman.Tab.AdminWatches",
   disabled : false, 
   permname : '', 
-  _tree : function()
+  _tree : function(_data)
   {
    var _this = this;
    var MODULE = this;
    return {
+   xtype : 'GridPanel',
    autoScroll : false,
    background : true,
    fitContainer : true,
@@ -43,9 +49,6 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
    region : 'center',
    tableName : 'core_watch',
    title : (function() {Roo.log('title') ;Roo.log(_this); return _this.title || "Watches"; })(),
-   xns : Roo,
-   '|xns' : 'Roo',
-   xtype : 'GridPanel',
    listeners : {
     activate : function() {
          _this.panel = this;
@@ -54,12 +57,12 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
          }
      }
    },
+   xns : Roo,
+   '|xns' : 'Roo',
    grid : {
+    xtype : 'Grid',
     autoExpandColumn : 'person_id_name',
     loadMask : true,
-    xns : Roo.grid,
-    '|xns' : 'Roo.grid',
-    xtype : 'Grid',
     listeners : {
      render : function() 
       {
@@ -79,27 +82,30 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
           }); 
       }
     },
+    xns : Roo.grid,
+    '|xns' : 'Roo.grid',
     footer : {
+     xtype : 'PagingToolbar',
      displayInfo : true,
      displayMsg : _this._strings['6ceb94ff48a58bd6d612b1f031d2c2ca'] /* Displaying core_watch{0} - {1} of {2} */,
      emptyMsg : _this._strings['a4e70e911022ccc98ab8055a09222cf2'] /* No core_watch found */,
      pageSize : 25,
      xns : Roo,
-     '|xns' : 'Roo',
-     xtype : 'PagingToolbar'
+     '|xns' : 'Roo'
     },
     toolbar : {
+     xtype : 'Toolbar',
      xns : Roo,
      '|xns' : 'Roo',
-     xtype : 'Toolbar',
      items  : [
       {
+       xtype : 'TextItem',
        text : _this._strings['498f79c4c5bbde77f1bceb6c86fd0f6d'] /* Show */,
        xns : Roo.Toolbar,
-       '|xns' : 'Roo.Toolbar',
-       xtype : 'TextItem'
+       '|xns' : 'Roo.Toolbar'
       },
       {
+       xtype : 'ComboBox',
        allowBlank : true,
        displayField : 'on_table',
        editable : false,
@@ -118,9 +124,6 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
        typeAhead : true,
        valueField : 'on_table',
        width : 150,
-       xns : Roo.form,
-       '|xns' : 'Roo.form',
-       xtype : 'ComboBox',
        listeners : {
         render : function (_self)
          {
@@ -131,12 +134,12 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
            _this.grid.footer.onClick('first');
          }
        },
+       xns : Roo.form,
+       '|xns' : 'Roo.form',
        store : {
+        xtype : 'Store',
         remoteSort : true,
         sortInfo : { field : 'on_table' , direction : 'ASC' },
-        xns : Roo.data,
-        '|xns' : 'Roo.data',
-        xtype : 'Store',
         listeners : {
          beforeload : function (_self, o)
           {
@@ -149,14 +152,17 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
               o.params._columns ='on_table';
           }
         },
+        xns : Roo.data,
+        '|xns' : 'Roo.data',
         proxy : {
+         xtype : 'HttpProxy',
          method : 'GET',
          url : baseURL + '/Roo/Events.php',
          xns : Roo.data,
-         '|xns' : 'Roo.data',
-         xtype : 'HttpProxy'
+         '|xns' : 'Roo.data'
         },
         reader : {
+         xtype : 'JsonReader',
          fields : [
              {
                  'name': 'id',
@@ -172,23 +178,20 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
          root : 'data',
          totalProperty : 'total',
          xns : Roo.data,
-         '|xns' : 'Roo.data',
-         xtype : 'JsonReader'
+         '|xns' : 'Roo.data'
         }
        }
       },
       {
+       xtype : 'Fill',
        xns : Roo.Toolbar,
-       '|xns' : 'Roo.Toolbar',
-       xtype : 'Fill'
+       '|xns' : 'Roo.Toolbar'
       },
       {
+       xtype : 'Button',
        cls : 'x-btn-text-icon',
        icon : Roo.rootURL + 'images/default/dd/drop-add.gif',
        text : _this._strings['ec211f7c20af43e742bf2570c3cb84f9'] /* Add */,
-       xns : Roo.Toolbar,
-       '|xns' : 'Roo.Toolbar',
-       xtype : 'Button',
        listeners : {
         click : function()
          {
@@ -199,30 +202,30 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
                  _this.grid.footer.onClick('first');
             }); 
          }
-       }
+       },
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
       },
       {
+       xtype : 'Button',
        cls : 'x-btn-text-icon',
        icon : rootURL + '/Pman/templates/images/trash.gif',
        text : _this._strings['f2a6c498fb90ee345d997f888fce3b18'] /* Delete */,
-       xns : Roo.Toolbar,
-       '|xns' : 'Roo.Toolbar',
-       xtype : 'Button',
        listeners : {
         click : function()
          {
               Pman.genericDelete(_this, 'core_watch'); 
          }
-       }
+       },
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
       }
      ]
     },
     dataSource : {
+     xtype : 'Store',
      remoteSort : true,
      sortInfo : { field : 'ontable', direction: 'ASC' },
-     xns : Roo.data,
-     '|xns' : 'Roo.data',
-     xtype : 'Store',
      listeners : {
       beforeload : function (_self, options)
        {
@@ -236,14 +239,17 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
                
        }
      },
+     xns : Roo.data,
+     '|xns' : 'Roo.data',
      proxy : {
+      xtype : 'HttpProxy',
       method : 'GET',
       url : baseURL + '/Roo/core_watch.php',
       xns : Roo.data,
-      '|xns' : 'Roo.data',
-      xtype : 'HttpProxy'
+      '|xns' : 'Roo.data'
      },
      reader : {
+      xtype : 'JsonReader',
       fields : [
           {
               'name': 'id',
@@ -350,12 +356,12 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
       root : 'data',
       totalProperty : 'total',
       xns : Roo.data,
-      '|xns' : 'Roo.data',
-      xtype : 'JsonReader'
+      '|xns' : 'Roo.data'
      }
     },
     colModel : [
      {
+      xtype : 'ColumnModel',
       dataIndex : 'event',
       header : _this._strings['c122d95a9c28f9a54baef2c7784bb038'] /* Watch Event */,
       renderer : function(v,x,r) { 
@@ -367,40 +373,47 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
       },
       width : 150,
       xns : Roo.grid,
-      '|xns' : 'Roo.grid',
-      xtype : 'ColumnModel'
+      '|xns' : 'Roo.grid'
      },
      {
+      xtype : 'ColumnModel',
       dataIndex : 'ontable',
       header : _this._strings['c4523f19258f444b936df7f96f57c7b9'] /* Watch Table */,
       renderer : function(v) { return String.format('{0}', v); },
       width : 150,
       xns : Roo.grid,
-      '|xns' : 'Roo.grid',
-      xtype : 'ColumnModel'
+      '|xns' : 'Roo.grid'
      },
      {
+      xtype : 'ColumnModel',
       dataIndex : 'onid',
       header : _this._strings['1a86349be7851cf03d6fe959b94ed6fb'] /* Watch ID */,
       renderer : function(v) { return String.format('{0}', v); },
       width : 75,
       xns : Roo.grid,
-      '|xns' : 'Roo.grid',
-      xtype : 'ColumnModel'
+      '|xns' : 'Roo.grid'
      },
      {
+      xtype : 'ColumnModel',
       dataIndex : 'person_id_name',
       header : _this._strings['b548a2ee926c118cc3211c5d8bb92a40'] /* Who get's notified */,
       renderer : function(v) { return String.format('{0}', v); },
       width : 150,
       xns : Roo.grid,
-      '|xns' : 'Roo.grid',
-      xtype : 'ColumnModel'
+      '|xns' : 'Roo.grid'
      },
      {
+      xtype : 'ColumnModel',
       dataIndex : 'medium',
       header : _this._strings['56abc507eda6d3226af192bf9b0e5d1f'] /* What happens */,
       renderer : function(v) {
+          var ar = v.split('::');
+          if (ar.length > 1) {
+              var act = ar[1].replace(/^notify/,'');
+      
+              return String.format('<span qtip="{0}">{1}<span>', v, act);
+          }
+          
           var ar = v.split(':');
           if (ar.length > 1) {
               var act = ar[1].replace(/^notify/,'');
@@ -412,17 +425,16 @@ Pman.Tab.AdminWatches = new Roo.XComponent({
        },
       width : 300,
       xns : Roo.grid,
-      '|xns' : 'Roo.grid',
-      xtype : 'ColumnModel'
+      '|xns' : 'Roo.grid'
      },
      {
+      xtype : 'ColumnModel',
       dataIndex : 'active',
       header : _this._strings['4d3d769b812b6faa6b76e1a8abaece2d'] /* Active */,
       renderer : function(v) { return String.format('{0}', v); },
       width : 75,
       xns : Roo.grid,
-      '|xns' : 'Roo.grid',
-      xtype : 'ColumnModel'
+      '|xns' : 'Roo.grid'
      }
     ]
    }
