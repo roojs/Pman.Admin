37419d12cfd184a1bf8dc80f9e795e1356e210aa
leon <leon@roojs.com>
2024-12-03 11:06:58 +0800

    Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.js

--- Pman.Dialog.AdminWatch.js
+++ Pman.Dialog.AdminWatch.js
@@ -336,7 +336,7 @@ Pman.Dialog.AdminWatch = {
               store : {
                xtype : 'SimpleStore',
                data : [0,1,2,3,4,5,6,7,8,9,11],
-               fields : [hour],
+               fields : ['hour'],
                xns : Roo.data,
                '|xns' : 'Roo.data'
               }
@@ -359,7 +359,7 @@ Pman.Dialog.AdminWatch = {
               store : {
                xtype : 'SimpleStore',
                data : [0,1,2,3,4,5,6,7,8,9,11,12],
-               fields : [minute],
+               fields : ['minute'],
                xns : Roo.data,
                '|xns' : 'Roo.data'
               }
