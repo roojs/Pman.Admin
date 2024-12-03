917ccdb91d0c9452e6a65c76827ccdb5fa7c1bb4
leon <leon@roojs.com>
2024-12-03 11:14:58 +0800

    Pman.Dialog.AdminWatch.bjs Pman.Dialog.AdminWatch.js

--- Pman.Dialog.AdminWatch.js
+++ Pman.Dialog.AdminWatch.js
@@ -328,7 +328,7 @@ Pman.Dialog.AdminWatch = {
              {
               xtype : 'ComboBox',
               displayField : 'hour',
-              hiddenName : '\"\"',
+              hiddenName : 'delay_h',
               listWidth : 100,
               mode : 'local',
               name : 'delay_hours',
