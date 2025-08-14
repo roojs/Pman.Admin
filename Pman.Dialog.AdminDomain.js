24148fb0e6a460cba7cb1030133ed0a2c80fc7a3
Alan <alan@roojs.com>
2025-08-14 15:40:47 +0800

    fix #8666 - pressrelease system seneding remote mx failure

---
+++ Pman.Dialog.AdminDomain.js
@@ -0,0 +1,242 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Roo.namespace('Pman.Dialog');
+
+Pman.Dialog.AdminDomain = {
+
+ _strings : {
+  '842c51ab0d597b4c7c61066fecf42b44' :"MX last updated",
+  'ea4788705e6873b424c65e91c2846b19' :"Cancel",
+  '22e6e4a2e35eb478f37738da66480192' :"Add / Edit Core Domain",
+  'eae639a70006feff484a39363c977e24' :"Domain",
+  'b331a153086a91e775f24c00de1f77d9' :"No mx record since",
+  '81c34401de67bbc904ea581fe1922c7b' :"Update Mx",
+  'c9cc8cce247e49bae79f15173ce97354' :"Save",
+  '289fe65c57825256edde389f99a1f05c' :"Has MX record"
+ },
+ _named_strings : {
+  'mx_updated_fieldLabel' : '842c51ab0d597b4c7c61066fecf42b44' /* MX last updated */ ,
+  'no_mx_dt_fieldLabel' : 'b331a153086a91e775f24c00de1f77d9' /* No mx record since */ ,
+  'domain_fieldLabel' : 'eae639a70006feff484a39363c977e24' /* Domain */ ,
+  'is_mx_valid_fieldLabel' : '289fe65c57825256edde389f99a1f05c' /* Has MX record */ 
+ },
+
+ dialog : false,
+ callback:  false,
+
+ show : function(data, cb)
+ {
+  if (!this.dialog) {
+   this.create();
+  }
+
+  this.callback = cb;
+  this.data = data;
+  this.dialog.show.apply(this.dialog,  Array.prototype.slice.call(arguments).slice(2));
+  if (this.form) {
+   this.form.reset();
+   this.form.setValues(data);
+   this.form.fireEvent('actioncomplete', this.form,  { type: 'setdata', data: data });
+  }
+
+ },
+
+ create : function()
+ {
+   var _this = this;
+   this.dialog = Roo.factory({
+    xtype : 'LayoutDialog',
+    background : true,
+    closable : false,
+    collapsible : false,
+    height : 175,
+    modal : true,
+    resizable : false,
+    title : _this._strings['22e6e4a2e35eb478f37738da66480192'] /* Add / Edit Core Domain */,
+    width : 500,
+    xns : Roo,
+    '|xns' : 'Roo',
+    center : {
+     xtype : 'LayoutRegion',
+     titlebar : false,
+     xns : Roo,
+     '|xns' : 'Roo'
+    },
+    buttons : [
+     {
+      xtype : 'Button',
+      text : _this._strings['81c34401de67bbc904ea581fe1922c7b'] /* Update Mx */,
+      listeners : {
+       click : function (_self, e)
+        {
+            var id = _this.form.findField("id").getValue();
+            
+            if(!(id * 1)) {
+                Roo.MessageBox.alert('Error', 'Please save first');
+                return;
+            }
+            
+            new Pman.Request({
+               url : baseURL + '/Roo/core_domain.php',
+               method : 'POST',
+               mask: "Updating",
+               params : {
+                   id : id,
+                   _update_mx : 1
+               }, 
+               success : function(res) {
+                    if(!res.data.length){
+                        return;
+                    }
+                    if(_this.data.id){
+                        _this.form.load({ method: 'GET', params: { '_id' : _this.data.id }});
+                    }
+               }
+           });
+        }
+      },
+      xns : Roo,
+      '|xns' : 'Roo'
+     },
+     {
+      xtype : 'Button',
+      text : _this._strings['ea4788705e6873b424c65e91c2846b19'] /* Cancel */,
+      listeners : {
+       click : function() {
+            _this.form.reset();
+            _this.dialog.hide();
+        }
+      },
+      xns : Roo,
+      '|xns' : 'Roo'
+     },
+     {
+      xtype : 'Button',
+      text : _this._strings['c9cc8cce247e49bae79f15173ce97354'] /* Save */,
+      listeners : {
+       click : function()
+        {
+            _this.dialog.el.mask("Saving");
+            _this.form.doAction('submit');
+            
+        }
+      },
+      xns : Roo,
+      '|xns' : 'Roo'
+     }
+    ],
+    items  : [
+     {
+      xtype : 'ContentPanel',
+      background : true,
+      fitToFrame : true,
+      region : 'center',
+      xns : Roo,
+      '|xns' : 'Roo',
+      items  : [
+       {
+        xtype : 'Form',
+        labelWidth : 180,
+        method : 'POST',
+        style : 'margin: 5px',
+        url : baseURL + '/Roo/Core_domain.php',
+        listeners : {
+         actioncomplete : function (_self, action)
+          {
+            if (action.type == 'setdata') {
+                 //_this.dialog.el.mask("Loading");
+                  
+                 this.load({ method: 'GET', params: { '_id' : _this.data.id }});
+                 return;
+              }
+              if (action.type == 'load') {
+                  _this.dialog.el.unmask();
+                  return;
+              }
+              if (action.type == 'submit' ) {
+                  _this.dialog.el.unmask();
+                  _this.dialog.hide();
+          
+                  if (_this.callback) {
+                     _this.callback.call(_this, _this.form.getValues());
+                  }
+                  _this.form.reset();
+              }
+          },
+         actionfailed : function (_self, action)
+          {
+              _this.dialog.el.unmask(); 
+             
+          },
+         rendered : function (form)
+          {
+             _this.form = form;
+          }
+        },
+        xns : Roo.form,
+        '|xns' : 'Roo.form',
+        items  : [
+         {
+          xtype : 'TextField',
+          allowBlank : false,
+          fieldLabel : _this._strings['eae639a70006feff484a39363c977e24'] /* Domain */,
+          name : 'domain',
+          width : 250,
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
+         },
+         {
+          xtype : 'DisplayField',
+          fieldLabel : _this._strings['b331a153086a91e775f24c00de1f77d9'] /* No mx record since */,
+          name : 'no_mx_dt',
+          valueRenderer : function()
+          {
+              if(!_this.form) {
+                  return '';
+              }
+              
+              var no_mx_dt = _this.form.findField('no_mx_dt').value;
+              
+              var ret = no_mx_dt instanceof Date ? no_mx_dt.format('Y-m-d H:i:s') : no_mx_dt;
+              
+              if(ret == '1000-01-01 00:00:00') {
+                  return 'N/A'
+              }
+              
+              return String.format("<span style='color:red;'>{0}</span>", ret);
+          },
+          width : 900,
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
+         },
+         {
+          xtype : 'DisplayField',
+          fieldLabel : _this._strings['842c51ab0d597b4c7c61066fecf42b44'] /* MX last updated */,
+          name : 'mx_updated',
+          width : 900,
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
+         },
+         {
+          xtype : 'Checkbox',
+          fieldLabel : _this._strings['289fe65c57825256edde389f99a1f05c'] /* Has MX record */,
+          name : 'is_mx_valid',
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
+         },
+         {
+          xtype : 'Hidden',
+          name : 'id',
+          xns : Roo.form,
+          '|xns' : 'Roo.form'
+         }
+        ]
+       }
+      ]
+     }
+    ]
+   });
+ }
+};
---
+++ Pman.Tab.AdminDomain.js
@@ -0,0 +1,235 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Roo.namespace('Pman.Tab');
+
+Pman.Tab.AdminDomain = new Roo.XComponent({
+
+ _strings : {
+  'ec211f7c20af43e742bf2570c3cb84f9' :"Add",
+  '13348442cc6a27032d2b4aa28b75a5d3' :"Search",
+  '7dce122004969d56ae2e0245cb754d35' :"Edit",
+  'eae639a70006feff484a39363c977e24' :"Domain",
+  'f2a6c498fb90ee345d997f888fce3b18' :"Delete"
+ },
+
+  part     :  ["Admin", "Domain" ],
+  order    : '9998-Pman.Tab.AdminDomain',
+  region   : 'center',
+  parent   : 'Pman.Tab.Admin',
+  name     : "Domain",
+  disabled : false, 
+  permname : '', 
+  _tree : function(_data)
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   xtype : 'Grid',
+   autoScroll : true,
+   background : true,
+   fitContainer : true,
+   fitToframe : true,
+   region : 'center',
+   tableName : 'core_domain',
+   title : _this._strings['eae639a70006feff484a39363c977e24'] /* Domain */,
+   listeners : {
+    activate : function() {
+         _this.domainPanel = this;
+     }
+   },
+   xns : Roo.panel,
+   '|xns' : 'Roo.panel',
+   grid : {
+    xtype : 'Grid',
+    autoExpandColumn : 'domain',
+    loadMask : true,
+    listeners : {
+     render : function() 
+      {
+          _this.domainGrid = this; 
+          if (_this.domainPanel.active) {
+             this.footer.onClick('first');
+          }
+      },
+     rowdblclick : function (_self, rowIndex, e)
+      {
+         var el = _this.domainGrid.ds.getAt(rowIndex);
+      
+         Pman.Dialog.AdminDomain.show({ id: el.id }, function() {
+              _this.domainGrid.footer.onClick('first');
+            });
+      }
+    },
+    xns : Roo.grid,
+    '|xns' : 'Roo.grid',
+    footer : {
+     xtype : 'PagingToolbar',
+     pageSize : 25,
+     xns : Roo,
+     '|xns' : 'Roo'
+    },
+    toolbar : {
+     xtype : 'Toolbar',
+     xns : Roo,
+     '|xns' : 'Roo',
+     items  : [
+      {
+       xtype : 'TextItem',
+       text : _this._strings['13348442cc6a27032d2b4aa28b75a5d3'] /* Search */,
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
+      },
+      {
+       xtype : 'TextField',
+       listeners : {
+        render : function (_self)
+         {
+             _this.searchBox = _self;
+         },
+        specialkey : function (_self, e)
+         {
+           if (e.getKey() == 13) {
+             _this.domainGrid.footer.onClick('first');
+           }
+         }
+       },
+       xns : Roo.form,
+       '|xns' : 'Roo.form'
+      },
+      {
+       xtype : 'Button',
+       cls : 'x-btn-icon',
+       icon : rootURL + '/Pman/templates/images/search.gif',
+       listeners : {
+        click : function (_self, e)
+         {
+         _this.domainGrid.footer.onClick('first');
+         }
+       },
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
+      },
+      {
+       xtype : 'Button',
+       cls : 'x-btn-icon',
+       icon : rootURL + '/Pman/templates/images/edit-clear.gif',
+       listeners : {
+        click : function (_self, e)
+         {
+             _this.searchBox.setValue('');
+             _this.domainGrid.footer.onClick('first');
+         }
+       },
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
+      },
+      {
+       xtype : 'Button',
+       text : _this._strings['ec211f7c20af43e742bf2570c3cb84f9'] /* Add */,
+       listeners : {
+        click : function()
+         {
+             Pman.Dialog.AdminDomain.show({ id:0 });
+         }
+       },
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
+      },
+      {
+       xtype : 'Button',
+       text : _this._strings['7dce122004969d56ae2e0245cb754d35'] /* Edit */,
+       listeners : {
+        click : function()
+         {
+             var el = _this.domainGrid.getSelections();
+             
+             if(el.length < 1){
+                 Roo.MessageBox.alert('Error', 'Please select the match first');
+                 return;
+             }
+             Pman.Dialog.AdminDomain.show({ id: el[0].id });
+         
+         }
+       },
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
+      },
+      {
+       xtype : 'Fill',
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
+      },
+      {
+       xtype : 'Button',
+       text : _this._strings['f2a6c498fb90ee345d997f888fce3b18'] /* Delete */,
+       listeners : {
+        click : function()
+         {
+             Pman.Delete.simple(_this.domainPanel, 'core_domain');
+         }
+       },
+       xns : Roo.Toolbar,
+       '|xns' : 'Roo.Toolbar'
+      }
+     ]
+    },
+    dataSource : {
+     xtype : 'Store',
+     remoteSort : true,
+     sortInfo : { field : 'domain', direction: 'ASC' },
+     listeners : {
+      beforeload : function (_self, options)
+       {
+           options.params = options.params || {};
+           options.params['query[domain]'] = _this.searchBox.getValue();
+       }
+     },
+     xns : Roo.data,
+     '|xns' : 'Roo.data',
+     proxy : {
+      xtype : 'HttpProxy',
+      method : 'GET',
+      url : baseURL + '/Roo/Core_domain.php',
+      xns : Roo.data,
+      '|xns' : 'Roo.data'
+     },
+     reader : {
+      xtype : 'JsonReader',
+      fields : [
+          {
+              'name': 'id',
+              'type': 'int'
+          },
+          {
+              'name': 'name',
+              'type': 'string'
+          },
+          {
+              'name': 'match_regex',
+              'type': 'string'
+          }
+      ],
+      id : 'id',
+      root : 'data',
+      totalProperty : 'total',
+      xns : Roo.data,
+      '|xns' : 'Roo.data'
+     }
+    },
+    colModel : [
+     {
+      xtype : 'ColumnModel',
+      dataIndex : 'domain',
+      header : _this._strings['eae639a70006feff484a39363c977e24'] /* Domain */,
+      renderer : function(v) { return String.format('{0}', v); },
+      sortable : true,
+      width : 200,
+      xns : Roo.grid,
+      '|xns' : 'Roo.grid'
+     }
+    ]
+   }
+  };  }
+});
