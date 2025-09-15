85ee6d3ee840ca59b6b838ab1462ff2972554d4b
Alan <alan@roojs.com>
2025-09-15 14:15:55 +0800

    fix #8666 - press release system sending report mx

--- Pman.Tab.AdminDomain.js
+++ Pman.Tab.AdminDomain.js
@@ -7,11 +7,17 @@ Roo.namespace('Pman.Tab');
 Pman.Tab.AdminDomain = new Roo.XComponent({
 
  _strings : {
-  'ec211f7c20af43e742bf2570c3cb84f9' :"Add",
   '13348442cc6a27032d2b4aa28b75a5d3' :"Search",
+  'ec211f7c20af43e742bf2570c3cb84f9' :"Add",
+  '79ebd90228ae192321ca47951612dea5' :"Displaying email domains {0} - {1} of {2}",
   '7dce122004969d56ae2e0245cb754d35' :"Edit",
+  '996d8bde90930fead44cf1d6989e532a' :"No email domain found",
+  '8f00afba26677da2da0b00ec8daf22bd' :"Email Domains",
   'eae639a70006feff484a39363c977e24' :"Domain",
-  'f2a6c498fb90ee345d997f888fce3b18' :"Delete"
+  '6aa48f150b0d29138ab848a1d4e6792c' :"No. of references",
+  'a181a603769c1f98ad927e7367c7aa51' :"all",
+  'f2a6c498fb90ee345d997f888fce3b18' :"Delete",
+  '9732d2a193579388e6d9138368554966' :"MX Last Updated"
  },
 
   part     :  ["Admin", "Domain" ],
@@ -33,7 +39,7 @@ Pman.Tab.AdminDomain = new Roo.XComponent({
    fitToframe : true,
    region : 'center',
    tableName : 'core_domain',
-   title : _this._strings['eae639a70006feff484a39363c977e24'] /* Domain */,
+   title : _this._strings['8f00afba26677da2da0b00ec8daf22bd'] /* Email Domains */,
    listeners : {
     activate : function() {
          _this.domainPanel = this;
@@ -66,7 +72,10 @@ Pman.Tab.AdminDomain = new Roo.XComponent({
     '|xns' : 'Roo.grid',
     footer : {
      xtype : 'PagingToolbar',
-     pageSize : 25,
+     displayInfo : true,
+     displayMsg : _this._strings['79ebd90228ae192321ca47951612dea5'] /* Displaying email domains {0} - {1} of {2} */,
+     emptyMsg : _this._strings['996d8bde90930fead44cf1d6989e532a'] /* No email domain found */,
+     pageSize : 50,
      xns : Roo,
      '|xns' : 'Roo'
     },
@@ -124,6 +133,75 @@ Pman.Tab.AdminDomain = new Roo.XComponent({
        },
        xns : Roo.Toolbar,
        '|xns' : 'Roo.Toolbar'
+      },
+      {
+       xtype : 'ComboBox',
+       allowBlank : true,
+       displayField : 'value',
+       editable : false,
+       mode : 'local',
+       triggerAction : 'all',
+       value : _this._strings['a181a603769c1f98ad927e7367c7aa51'] /* all */,
+       valueField : 'key',
+       width : 100,
+       listeners : {
+        render : function (_self)
+         {
+             _this.statusSel = _self;
+         },
+        select : function (combo, record, index)
+         {
+             _this.domainGrid.footer.onClick('first');
+         }
+       },
+       xns : Roo.form,
+       '|xns' : 'Roo.form',
+       store : {
+        xtype : 'SimpleStore',
+        data : [
+            
+             [ 'all', 'All' ],
+             [ 'valid_mx', "Valid MX" ],
+             [ 'invalid_mx', "Invalid MX" ]
+        ],
+        fields : [ 'key', 'value'],
+        xns : Roo.data,
+        '|xns' : 'Roo.data'
+       }
+      },
+      {
+       xtype : 'ComboBox',
+       allowBlank : true,
+       displayField : 'value',
+       editable : false,
+       mode : 'local',
+       triggerAction : 'all',
+       value : _this._strings['a181a603769c1f98ad927e7367c7aa51'] /* all */,
+       valueField : 'key',
+       width : 125,
+       listeners : {
+        render : function (_self)
+         {
+             _this.referenceSel = _self;
+         },
+        select : function (combo, record, index)
+         {
+             _this.domainGrid.footer.onClick('first');
+         }
+       },
+       xns : Roo.form,
+       '|xns' : 'Roo.form',
+       store : {
+        xtype : 'SimpleStore',
+        data : [
+             [ 'all', 'All' ],
+             [ 'with_references', "Has References" ],
+             [ 'without_reference', "No Reference" ]
+        ],
+        fields : [ 'key', 'value'],
+        xns : Roo.data,
+        '|xns' : 'Roo.data'
+       }
       },
       {
        xtype : 'Button',
@@ -184,6 +262,9 @@ Pman.Tab.AdminDomain = new Roo.XComponent({
        {
            options.params = options.params || {};
            options.params['query[domain]'] = _this.searchBox.getValue();
+           options.params['_status'] = _this.statusSel.getValue();
+           options.params['_with_reference_count'] = 1;
+           options.params['_reference_status'] = _this.referenceSel.getValue();
        }
      },
      xns : Roo.data,
@@ -223,9 +304,46 @@ Pman.Tab.AdminDomain = new Roo.XComponent({
       xtype : 'ColumnModel',
       dataIndex : 'domain',
       header : _this._strings['eae639a70006feff484a39363c977e24'] /* Domain */,
-      renderer : function(v) { return String.format('{0}', v); },
+      renderer : function(v, x, r) {
+          var format = "{0}";
+          if(
+              !(r.data.has_mx * 1) && 
+              r.data.mx_updated.format('Y-m-d H:i:s') !== '1000-01-01 00:00:00'
+          ) {
+              format = "<span style='color:red;'>{0}</span>";
+              var noMxDt = r.data.no_mx_dt.format('Y-m-d H:i:s'); 
+              if(noMxDt !== '1000-01-01 00:00:00') {
+                  format = "<span style='color:red;' qtip ='No MX since " + noMxDt + "'>{0}</span>";
+              }
+          }
+          return String.format(format, v);
+      },
       sortable : true,
       width : 200,
+      xns : Roo.grid,
+      '|xns' : 'Roo.grid'
+     },
+     {
+      xtype : 'ColumnModel',
+      dataIndex : 'mx_updated',
+      header : _this._strings['9732d2a193579388e6d9138368554966'] /* MX Last Updated */,
+      renderer : function(v) {
+          return String.format('{0}', v ? v.format("Y-m-d H:i:s")  : ''); 
+      },
+      sortable : true,
+      width : 120,
+      xns : Roo.grid,
+      '|xns' : 'Roo.grid'
+     },
+     {
+      xtype : 'ColumnModel',
+      dataIndex : 'person_reference_count',
+      header : _this._strings['6aa48f150b0d29138ab848a1d4e6792c'] /* No. of references */,
+      renderer : function(v) {
+          return String.format('{0}', v); 
+      },
+      sortable : true,
+      width : 100,
       xns : Roo.grid,
       '|xns' : 'Roo.grid'
      }
