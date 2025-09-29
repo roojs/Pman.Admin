f21b7f754f17da7a920b40a449795fc0403a0a1a
leon <leon@roojs.com>
2025-09-29 16:29:09 +0800

    Pman.Tab.CoreNotifyServers.bjs Pman.Tab.CoreNotifyServers.js

--- Pman.Tab.CoreNotifyServers.js
+++ Pman.Tab.CoreNotifyServers.js
@@ -22,7 +22,6 @@ Pman.Tab.CoreNotifyServers = new Roo.XComponent({
   'b26686c0a708faee42861d8b905e882e' :"Last Sent",
   '1203cd27e4d1ab6f1296728c021d9c1a' :"Is Active",
   'def36b726efed529b13ba240dd331a12' :"Pool",
-  'c9d03748d1a54666b5c7a5187109301b' :"Delete Selected File or  Image",
   'be6838286e448ad65c5b55d690e2c38b' :"In Queue",
   '902b0d55fddef6f8d651fe1035b7d4bd' :"Error",
   '8e16a71b3d8217eb80b39b7d8dec4296' :"Image Type",
@@ -30,12 +29,10 @@ Pman.Tab.CoreNotifyServers = new Roo.XComponent({
   'eae639a70006feff484a39363c977e24' :"Domain",
   'e3974584afa867d8619253bc669d6197' :"Notify Servers",
   '825bd435c12978e8492330c2a0d823db' :"Helo",
-  '1d05bbecb1dd9127ccc9a5efaf492a2c' :"crm_person",
   'b18c29b8470190a02813415a04a2191f' :"Filesize",
   '55cfeeacad2f92b9fea0a1bbb6449fac' :"Update Image Details",
   '1063e38cb53d94d386f21227fcd84717' :"Remove",
-  'c8f4b8c435b3d99a66e1b91bec60737c' :"Hostname",
-  'ea72bacd2fdfa818907bb9559e6905a1' :"Upload Image or File"
+  'c8f4b8c435b3d99a66e1b91bec60737c' :"Hostname"
  },
  _named_strings : {
   'imgtype_fieldLabel' : '8e16a71b3d8217eb80b39b7d8dec4296' /* Image Type */ ,
@@ -43,8 +40,6 @@ Pman.Tab.CoreNotifyServers = new Roo.XComponent({
   'imgtype_qtip' : '471ddaf9e80da04dd5a3a54daa0239b0' /* Select Image Type */ ,
   'imgtype_loadingText' : '1243daf593fa297e07ab03bf06d925af' /* Searching... */ ,
   'mimetype_fieldLabel' : '529701d5ccccf8a1c604c27f48ce7d82' /* Mimetype */ ,
-  'imageUpload_fieldLabel' : 'ea72bacd2fdfa818907bb9559e6905a1' /* Upload Image or File */ ,
-  'ontable_value' : '1d05bbecb1dd9127ccc9a5efaf492a2c' /* crm_person */ ,
   'imgtype_emptyText' : '8535bcc0f05358a583bb432bbadf7e0d' /* Select type */ ,
   'filesize_fieldLabel' : 'b18c29b8470190a02813415a04a2191f' /* Filesize */ 
  },
@@ -157,281 +152,7 @@ Pman.Tab.CoreNotifyServers = new Roo.XComponent({
         xns : Roo,
         '|xns' : 'Roo'
        },
-       south : {
-        xtype : 'LayoutRegion',
-        height : 60,
-        xns : Roo,
-        '|xns' : 'Roo'
-       },
        items  : [
-        {
-         xtype : 'ContentPanel',
-         autoScroll : true,
-         fitContainer : true,
-         fitToFrame : true,
-         region : 'center',
-         xns : Roo,
-         '|xns' : 'Roo',
-         toolbar : {
-          xtype : 'Toolbar',
-          xns : Roo,
-          '|xns' : 'Roo',
-          items  : [
-           {
-            xtype : 'Fill',
-            xns : Roo.Toolbar,
-            '|xns' : 'Roo.Toolbar'
-           },
-           {
-            xtype : 'Button',
-            text : _this._strings['c9d03748d1a54666b5c7a5187109301b'] /* Delete Selected File or  Image */,
-            listeners : {
-             click : function (_self, e)
-              {
-                  if (!_this.imageView.getSelectedNodes().length) {
-                   Roo.MessageBox.alert("Error", "Select an Image");
-                   return;
-                 }
-                 var id  = _this.imageView.getNodeData(_this.imageView.getSelectedNodes()[0]).id;
-                 _this.imageView.el.mask('Deleting');
-                 
-                 Roo.Ajax.request({
-                    url : baseURL + '/Roo/Images.php',
-                    params : {
-                       _delete : id
-                    },
-                    method : 'POST',
-                    success : function() {
-                        _this.imageView.loadImages();
-                    },
-                    failure : function() {
-                         _this.imageView.el.unmask();
-                         Roo.MessageBox.alert("Error", "Error deleting image");
-                         return;
-                    }
-                });
-               }
-            },
-            xns : Roo.Toolbar,
-            '|xns' : 'Roo.Toolbar'
-           }
-          ]
-         },
-         items  : [
-          {
-           xtype : 'JsonView',
-           jsonRoot : 'data',
-           loadImages : function() {
-               var imgbase = typeof(publicBaseURL) != 'undefined' ? publicBaseURL : rootURL + '/crm.php';
-           
-                this.load({
-                   url : baseURL + '/Roo/Images.php',
-                   method : 'GET',
-                   params : {
-                      ontable : 'crm_person',
-                      onid : _this.viewPerson.id,
-                      'query[imagesize]' : '150x150',
-                      'query[imageBaseURL]' : imgbase,
-                      limit : 150
-                   },
-                   text : 'Loading'
-              });
-              
-           },
-           singleSelect : true,
-           listeners : {
-            beforerender : function (_self, data)
-             {
-                _this.imageView = _self;
-                
-                if (this.stylesheet) {
-                   return;
-                }
-                this.el.addClass('thumb-browser');
-             
-                
-                this.stylesheet = Roo.util.CSS.createStyleSheet(
-                     {
-                       '.thumb-browser .thumb-wrap' : {
-                             border:'3px solid #EEE',
-                             'float':'left',
-                             height:'190px',
-                             margin:'2px',
-                             'padding-left':'5px',
-                             'padding-top':'5px',
-                             width:'160px'
-                         },
-                         '.thumb-browser .x-view-selected' : {
-                           border:'3px solid #333'
-                       
-                         },
-                         '.thumblabel' : {
-                             'font-size': '11px',
-                             'font-family' : 'tahoma, verdana, helvetica',
-                             'max-width': '153px',
-                                 'overflow': 'hidden',
-                                 'white-space': 'nowrap'
-                         }
-             
-                     }, Roo.id()
-                     );
-                
-             },
-            dblclick : function (_self, index, node, e)
-             {
-                    
-                    if (!node) {
-                     return;
-                     }
-                    var snode = false;
-                    try {
-                         snode =  _this.imageView.getNodeData(node);
-                     } catch (e) {}
-                     
-                      
-                     if (!snode) {
-                         return;
-                     }
-                     new Pman.Download({
-                         url : baseURL + '/Images/Download/' + snode.id + '/'  + snode.shorten_name
-                       }); 
-             },
-            selectionchange : function (_self, selections)
-             {
-                  
-                  var snode = false;
-                  if (selections && selections.length) {
-                     try {
-                        var snode =  _this.imageView.getNodeData(selections[0]);
-                     } catch (e) {}
-                 }
-                  
-                 if (!snode) {
-                      _this.imagePanel.layout.getRegion('east').hide();
-                     return;
-                 }
-                 _this.imagePanel.layout.getRegion('east').show();    
-              
-                 _this.imageForm.setValues(snode);
-             }
-           },
-           xns : Roo,
-           '|xns' : 'Roo',
-           tpl : {
-            xtype : 'Template',
-            html : '<div class="thumb-wrap"> ' + 
-              '<div class="thumb"><img  src="{url_thumb}" class="thumb-img"></div>' + 
-              '<div class="thumblabel" qtip="{linkurl}">{title}<br/>{linkurl}</div>' + 
-            '</div>',
-            xns : Roo,
-            '|xns' : 'Roo'
-           }
-          }
-         ]
-        },
-        {
-         xtype : 'ContentPanel',
-         region : 'south',
-         xns : Roo,
-         '|xns' : 'Roo',
-         items  : [
-          {
-           xtype : 'Form',
-           fileUpload : true,
-           labelWidth : 200,
-           progressUrl : baseURL  + '/Core/UploadProgress.php',
-           style : 'margin: 10px;',
-           url : baseURL + '/Roo/Images.php',
-           listeners : {
-            actioncomplete : function (_self, action)
-             {
-             // only action this will handle is submit?
-             // should test for errors..
-                 if (action.failureType) {
-                     _this.uploadForm.el.unmask();
-                     _this.uploadSending = false;
-                     Roo.MessageBox.alert("Error", "Upload Failed");
-                     return false;
-                 
-                 }
-                   
-                 
-                 if (action.type =='submit') {
-                      _this.uploadForm.reset();
-                        _this.uploadSending = false;
-                       _this.uploadForm.el.unmask();         
-                       _this.imageView.loadImages();
-                      return;
-                 }
-                  
-             },
-            actionfailed : function (_self, action)
-             {
-             
-                _this.uploadForm.el.unmask();
-                _this.uploadForm.reset();
-                 _this.uploadSending = false;
-                 Roo.MessageBox.alert("Error", "Upload Failed");
-                 _this.imageView.loadImages();
-                 
-               
-             },
-            rendered : function (form)
-             {
-               _this.uploadForm = form;
-             }
-           },
-           xns : Roo.form,
-           '|xns' : 'Roo.form',
-           items  : [
-            {
-             xtype : 'Row',
-             xns : Roo.form,
-             '|xns' : 'Roo.form',
-             items  : [
-              {
-               xtype : 'Column',
-               width : 600,
-               xns : Roo.form,
-               '|xns' : 'Roo.form',
-               items  : [
-                {
-                 xtype : 'TextField',
-                 allowBlank : false,
-                 fieldLabel : _this._strings['ea72bacd2fdfa818907bb9559e6905a1'] /* Upload Image or File */,
-                 inputType : 'file',
-                 name : 'imageUpload',
-                 width : 250,
-                 listeners : {
-                  change : function (_self, newValue, oldValue)
-                   {
-                       
-                   }
-                 },
-                 xns : Roo.form,
-                 '|xns' : 'Roo.form'
-                },
-                {
-                 xtype : 'Hidden',
-                 name : 'onid',
-                 xns : Roo.form,
-                 '|xns' : 'Roo.form'
-                },
-                {
-                 xtype : 'Hidden',
-                 name : 'ontable',
-                 value : _this._strings['1d05bbecb1dd9127ccc9a5efaf492a2c'] /* crm_person */,
-                 xns : Roo.form,
-                 '|xns' : 'Roo.form'
-                }
-               ]
-              }
-             ]
-            }
-           ]
-          }
-         ]
-        },
         {
          xtype : 'ContentPanel',
          region : 'east',
